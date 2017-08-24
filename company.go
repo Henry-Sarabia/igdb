3e3338ba8f197a838323e2e0f22b65c8a9476dd8
Henry-Sarabia <henrysarabia@mail.fresnostate.edu>
2017-08-24 01:36:55 -0700

    Add Company types

---
+++ company.go
@@ -0,0 +1,26 @@
+package main
+
+// CountryCode code ISO-3316-1
+type CountryCode int
+
+// Company is
+type Company struct {
+	ID                 ID           `json:"ID"`
+	Name               string       `json:"name"`
+	Slug               string       `json:"slug"`
+	URL                URL          `json:"url"`
+	CreatedAt          int          `json:"created_at"`
+	UpdatedAt          int          `json:"updated_at"`
+	Logo               Image        `json:"logo"`
+	Description        string       `json:"description"`
+	Country            CountryCode  `json:"country"`
+	Website            string       `json:"website"`
+	StartDate          int          `json:"start_date"` //unix epoch
+	StartDateCategory  DateCategory `json:"start_date_category"`
+	ChangedID          ID           `json:"changed_company_id"`
+	ChangeDate         int          `json:"changed_date"` //unix epoch
+	ChangeDateCategory DateCategory `json:"change_date_category"`
+	Twitter            string       `json:"twitter"`
+	Published          []ID         `json:"published"`
+	Developed          []ID         `json:"developed"`
+}
