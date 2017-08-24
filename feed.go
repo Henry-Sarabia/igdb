e517e2bd6c463787f272e2eddf3c8d4aceb7e1f4
Henry-Sarabia <henrysarabia@mail.fresnostate.edu>
2017-08-24 01:47:59 -0700

    Add feed types

---
+++ feed.go
@@ -0,0 +1,18 @@
+package main
+
+// FeedCategory codes
+type FeedCategory int
+
+// Feed is
+type Feed struct {
+	ID        ID           `json:"id"`
+	URL       URL          `json:"url"`
+	CreatedAt int          `json:"created_at"` //unix epoch
+	UpdatedAt int          `json:"updated_at"` //unix epoch
+	Content   string       `json:"content"`
+	Category  FeedCategory `json:"category"`
+	User      ID           `json:"user"`
+	Games     []ID         `json:"games"`
+	Title     string       `json:"title"`
+	LikeCount int          `json:"feed_likes_count"`
+}
