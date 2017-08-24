9bc51af128472784ed10d8aae8d3403822a2dbe6
Henry-Sarabia <henrysarabia@mail.fresnostate.edu>
2017-08-24 01:34:49 -0700

    Add character types

---
+++ character.go
@@ -0,0 +1,23 @@
+package main
+
+// GenderCode codes
+type GenderCode int
+
+// SpeciesCode codes
+type SpeciesCode int
+
+// Character is
+type Character struct {
+	ID        ID          `json:"id"`
+	Name      string      `json:"name"`
+	Slug      string      `json:"slug"`
+	URL       URL         `json:"url"`
+	CreatedAt int         `json:"created_at"`
+	UpdatedAt int         `json:"updated_at"`
+	MugShot   Image       `json:"mug_shot"`
+	Gender    GenderCode  `json:"gender"`
+	AKAs      []string    `json:"akas"`
+	Species   SpeciesCode `json:"species"`
+	Games     []ID        `json:"games"`
+	People    []ID        `json:"people"`
+}
