6ab80b318c988f179e976999535faa3d06119413
Henry-Sarabia <henrysarabia@mail.fresnostate.edu>
2017-08-25 02:46:52 -0700

    Add theme type

---
+++ theme.go
@@ -0,0 +1,12 @@
+package igdb
+
+// Theme type
+type Theme struct {
+	ID        ID     `json:"id"`
+	Name      string `json:"name"`
+	Slug      string `json:"slug"`
+	URL       URL    `json:"url"`
+	CreatedAt int    `json:"created_at"` //unix epoch
+	UpdatedAt int    `json:"updated_at"` //unix epoch
+	Games     []ID   `json:"games"`
+}
