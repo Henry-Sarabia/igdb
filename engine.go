877a7e56a5e9aa94ae1255f90975a0fd516302cd
Henry-Sarabia <henrysarabia@mail.fresnostate.edu>
2017-08-24 17:49:10 -0700

    Add Engine type

---
+++ engine.go
@@ -0,0 +1,15 @@
+package main
+
+// Engine is
+type Engine struct {
+	ID        ID     `json:"id"`
+	Name      string `json:"name"`
+	Slug      string `json:"slug"`
+	URL       URL    `json:"url"`
+	CreatedAt int    `json:"created_at"` //unix epoch
+	UpdatedAt int    `json:"updated_at"` //unix epoch
+	Logo      Image  `json:"logo"`
+	Games     []ID   `json:"games"`
+	Companies []ID   `json:"companies"`
+	Platforms []ID   `json:"platforms"`
+}
