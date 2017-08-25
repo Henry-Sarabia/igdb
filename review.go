3099767c7833c608b51caee6b2bb993b24aedee4
Henry-Sarabia <henrysarabia@mail.fresnostate.edu>
2017-08-25 02:45:46 -0700

    Add review type

---
+++ review.go
@@ -0,0 +1,24 @@
+package igdb
+
+// Review type
+type Review struct {
+	ID             ID     `json:"id"`
+	Username       string `json:"username"`
+	Name           string `json:"name"`
+	Slug           string `json:"slug"`
+	URL            URL    `json:"url"`
+	CreatedAt      int    `json:"created_at"` //unix epoch
+	UpdatedAt      int    `json:"updated_at"` //unix epoch
+	Game           ID     `json:"game"`
+	Category       int    `json:"category"` // Documentation is missing
+	Likes          int    `json:"likes"`
+	Views          int    `json:"views"`
+	RatingCategory int    `json:"rating_category"` // Documenation is missing
+	Platform       ID     `json:"platform"`
+	Video          string `json:"video"`
+	Introduction   string `json:"introduction"`
+	Content        string `json:"content"`
+	Conclusion     string `json:"conclusion"`
+	PositivePoints string `json:"positive_points"`
+	NegativePoints string `json:"negative_points"`
+}
