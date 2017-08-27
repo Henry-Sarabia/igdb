91cf1794aa6befbc2ed880091f466836efa2813e
Henry-Sarabia <henrysarabia@mail.fresnostate.edu>
2017-08-26 20:13:17 -0700

    Add get game functionality

--- game.go
+++ game.go
@@ -1,34 +1,52 @@
 package igdb
 
-// GameCategory code
+import (
+	"fmt"
+	"strconv"
+)
+
+// GameCategory is a code associated with a game
+// denoting its category. The codes are as follows:
+// 0 - Main Game
+// 1 - DLC / Addon
+// 2 - Expansion
+// 3 - Bundle
+// 4 - Standalone Expansion
 type GameCategory int
 
-// BeatTime is the time to beat a given game measured in seconds.
+// BeatTime is the time to beat a game
+// measured in seconds.
 type BeatTime struct {
 	Hastly     int `json:"hastly"`
 	Normally   int `json:"normally"`
 	Completely int `json:"completely"`
 }
 
-// ESRB is
+// ESRB containts the rating and synopsis
+// of a game from the Entertainment Software
+// Rating Board.
 type ESRB struct {
 	Rating   int    `json:"rating"`
 	Synopsis string `json:"synopsis"`
 }
 
-// PEGI is
+// PEGI contains the rating and synopsis
+// of a game from the Pan European Game
+// Information system.
 type PEGI struct {
 	Rating   int    `json:"rating"`
 	Synopsis string `json:"synopsis"`
 }
 
-// Website is
+// Website contains information about
+// a website referenced in the IGDB.
 type Website struct {
 	Category int `json:"category"` //codes
 	URL      URL `json:"url"`
 }
 
-// Game is
+// Game contains information about a game stored in the IGDB.
+// See https://igdb.github.io/api/endpoints/game/ for more information.
 type Game struct {
 	ID                   ID            `json:"id"`
 	Name                 string        `json:"name"`
@@ -66,7 +84,7 @@ type Game struct {
 	AlternativeNames     []AltName     `json:"alternative_names"`
 	Screenshots          []Image       `json:"screenshots"`
 	Videos               []Video       `json:"videos"`
-	Covers               []Image       `json:"cover"`
+	Covers               Image         `json:"cover"`
 	ESRB                 ESRB          `json:"esrb"`
 	PEGI                 PEGI          `json:"pegi"`
 	Websites             []Website     `json:"websites"`
@@ -76,4 +94,20 @@ type Game struct {
 	Standalone           []ID          `json:"standalone_expansions"`
 	Bundles              []ID          `json:"bundles"`
 	SimilarGames         []ID          `json:"games"`
+}
+
+// GetGame gets IGDB information for a game identified
+// by their unique IGDB ID.
+func (c *Client) GetGame(id ID) (*Game, error) {
+	url := rootURL + "games/" + strconv.Itoa(int(id))
+	fmt.Println(url)
+
+	var g []Game
+
+	err := c.get(url, &g)
+	if err != nil {
+		return nil, err
+	}
+
+	return &g[0], nil
 }
