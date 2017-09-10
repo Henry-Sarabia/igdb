a5ab705a3881c38ca1b2ca5ac26aef2af0c1660a
Henry-Sarabia <henrysarabia@mail.fresnostate.edu>
2017-09-09 18:13:18 -0700

    Add credit fetching functionality

--- credits.go
+++ credits.go
@@ -1,10 +1,15 @@
 package igdb
 
+import (
+	"strconv"
+	"strings"
+)
+
 // CreditCategory codes
 type CreditCategory int
 
-// Credits is
-type Credits struct {
+// Credit is
+type Credit struct {
 	ID                    int            `json:"id"`
 	Name                  string         `json:"name"`
 	Slug                  string         `json:"slug"`
@@ -21,4 +26,79 @@ type Credits struct {
 	Country               CountryCode    `json:"country"`
 	CreditedName          string         `json:"credited_name"`
 	CharacterCreditedName string         `json:"character_credited_name"`
+}
+
+// GetCredit gets IGDB information for a credit identified by its unique IGDB ID.
+func (c *Client) GetCredit(id int, opts ...OptionFunc) (*Credit, error) {
+	opt := newOpt()
+	for _, optFunc := range opts {
+		optFunc(&opt)
+	}
+
+	url := c.rootURL + "credits/" + strconv.Itoa(id)
+	if opts != nil {
+		if values := opt.Values.Encode(); values != "" {
+			url += "?" + values
+		}
+	}
+
+	var cr []Credit
+
+	err := c.get(url, &cr)
+	if err != nil {
+		return nil, err
+	}
+
+	return &cr[0], nil
+}
+
+// GetCredits gets IGDB information for a list of credits identified by their
+// unique IGDB IDs.
+func (c *Client) GetCredits(ids []int, opts ...OptionFunc) ([]*Credit, error) {
+	opt := newOpt()
+	for _, optFunc := range opts {
+		optFunc(&opt)
+	}
+
+	str := intsToString(ids)
+	url := c.rootURL + "credits/" + strings.Join(str, ",")
+	if opts != nil {
+		if values := opt.Values.Encode(); values != "" {
+			url += "?" + values
+		}
+	}
+
+	var cr []*Credit
+
+	err := c.get(url, &cr)
+	if err != nil {
+		return nil, err
+	}
+
+	return cr, nil
+}
+
+// SearchCredits searches the IGDB using the given query and returns IGDB information
+// for the results. Use functional options for pagination and to sort results by parameter.
+func (c *Client) SearchCredits(qry string, opts ...OptionFunc) ([]*Credit, error) {
+	opt := newOpt()
+	for _, optFunc := range opts {
+		optFunc(&opt)
+	}
+
+	url := c.rootURL + "credits/?search=" + qry
+	if opts != nil {
+		if values := opt.Values.Encode(); values != "" {
+			url += "&" + values
+		}
+	}
+
+	var cr []*Credit
+
+	err := c.get(url, &cr)
+	if err != nil {
+		return nil, err
+	}
+
+	return cr, nil
 }
