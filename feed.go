5d10035d286d66cd2a47ac4e840b2e99718927ee
Henry-Sarabia <henrysarabia@mail.fresnostate.edu>
2017-09-10 21:54:05 -0700

    Add Feed fetching functionality

--- feed.go
+++ feed.go
@@ -1,5 +1,10 @@
 package igdb
 
+import (
+	"strconv"
+	"strings"
+)
+
 // FeedCategory codes
 type FeedCategory int
 
@@ -15,4 +20,79 @@ type Feed struct {
 	Games     []int        `json:"games"`
 	Title     string       `json:"title"`
 	LikeCount int          `json:"feed_likes_count"`
+}
+
+// GetFeed gets IGDB information for a feed identified by its unique IGDB ID.
+func (c *Client) GetFeed(id int, opts ...OptionFunc) (*Feed, error) {
+	opt := newOpt()
+	for _, optFunc := range opts {
+		optFunc(&opt)
+	}
+
+	url := c.rootURL + "feeds/" + strconv.Itoa(id)
+	if opts != nil {
+		if values := opt.Values.Encode(); values != "" {
+			url += "?" + values
+		}
+	}
+
+	var f []Feed
+
+	err := c.get(url, &f)
+	if err != nil {
+		return nil, err
+	}
+
+	return &f[0], nil
+}
+
+// GetFeeds gets IGDB information for a list of game engines identified by their
+// unique IGDB IDs.
+func (c *Client) GetFeeds(ids []int, opts ...OptionFunc) ([]*Feed, error) {
+	opt := newOpt()
+	for _, optFunc := range opts {
+		optFunc(&opt)
+	}
+
+	str := intsToString(ids)
+	url := c.rootURL + "feeds/" + strings.Join(str, ",")
+	if opts != nil {
+		if values := opt.Values.Encode(); values != "" {
+			url += "?" + values
+		}
+	}
+
+	var f []*Feed
+
+	err := c.get(url, &f)
+	if err != nil {
+		return nil, err
+	}
+
+	return f, nil
+}
+
+// SearchFeeds searches the IGDB using the given query and returns IGDB information
+// for the results. Use functional options for pagination and to sort results by parameter.
+func (c *Client) SearchFeeds(qry string, opts ...OptionFunc) ([]*Feed, error) {
+	opt := newOpt()
+	for _, optFunc := range opts {
+		optFunc(&opt)
+	}
+
+	url := c.rootURL + "feeds/?search=" + qry
+	if opts != nil {
+		if values := opt.Values.Encode(); values != "" {
+			url += "&" + values
+		}
+	}
+
+	var f []*Feed
+
+	err := c.get(url, &f)
+	if err != nil {
+		return nil, err
+	}
+
+	return f, nil
 }
