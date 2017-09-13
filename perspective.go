ed8edf6c23bf116e96a8a3ed63f21ecd9202a9cb
Henry-Sarabia <henrysarabia@mail.fresnostate.edu>
2017-09-12 19:44:16 -0700

    Add Perspective fetching functionality

--- perspective.go
+++ perspective.go
@@ -1,5 +1,10 @@
 package igdb
 
+import (
+	"strconv"
+	"strings"
+)
+
 // Perspective type
 type Perspective struct {
 	ID        int    `json:"id"`
@@ -9,4 +14,79 @@ type Perspective struct {
 	CreatedAt int    `json:"created_at"` //unix epoch
 	UpdatedAt int    `json:"updated_at"` //unix epoch
 	Games     []int  `json:"games"`
+}
+
+// GetPerspective gets IGDB information for a player perspective identified by its unique IGDB ID.
+func (c *Client) GetPerspective(id int, opts ...OptionFunc) (*Perspective, error) {
+	opt := newOpt()
+	for _, optFunc := range opts {
+		optFunc(&opt)
+	}
+
+	url := c.rootURL + "player_perspectives/" + strconv.Itoa(id)
+	if opts != nil {
+		if values := opt.Values.Encode(); values != "" {
+			url += "?" + values
+		}
+	}
+
+	var p []Perspective
+
+	err := c.get(url, &p)
+	if err != nil {
+		return nil, err
+	}
+
+	return &p[0], nil
+}
+
+// GetPerspectives gets IGDB information for a list of player perspectives identified by their
+// unique IGDB IDs.
+func (c *Client) GetPerspectives(ids []int, opts ...OptionFunc) ([]*Perspective, error) {
+	opt := newOpt()
+	for _, optFunc := range opts {
+		optFunc(&opt)
+	}
+
+	str := intsToString(ids)
+	url := c.rootURL + "player_perspectives/" + strings.Join(str, ",")
+	if opts != nil {
+		if values := opt.Values.Encode(); values != "" {
+			url += "?" + values
+		}
+	}
+
+	var p []*Perspective
+
+	err := c.get(url, &p)
+	if err != nil {
+		return nil, err
+	}
+
+	return p, nil
+}
+
+// SearchPerspectives searches the IGDB using the given query and returns IGDB information
+// for the results. Use functional options for pagination and to sort results by parameter.
+func (c *Client) SearchPerspectives(qry string, opts ...OptionFunc) ([]*Perspective, error) {
+	opt := newOpt()
+	for _, optFunc := range opts {
+		optFunc(&opt)
+	}
+
+	url := c.rootURL + "player_perspectives/?search=" + qry
+	if opts != nil {
+		if values := opt.Values.Encode(); values != "" {
+			url += "&" + values
+		}
+	}
+
+	var p []*Perspective
+
+	err := c.get(url, &p)
+	if err != nil {
+		return nil, err
+	}
+
+	return p, nil
 }
