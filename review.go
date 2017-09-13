a1ee30fdcb3d077401d1c0eced33a68faa32fa8b
Henry-Sarabia <henrysarabia@mail.fresnostate.edu>
2017-09-13 15:19:31 -0700

    Add Review fetching functionality

--- review.go
+++ review.go
@@ -1,5 +1,10 @@
 package igdb
 
+import (
+	"strconv"
+	"strings"
+)
+
 // Review type
 type Review struct {
 	ID             int    `json:"id"`
@@ -21,4 +26,81 @@ type Review struct {
 	Conclusion     string `json:"conclusion"`
 	PositivePoints string `json:"positive_points"`
 	NegativePoints string `json:"negative_points"`
+}
+
+// GetReview gets IGDB information for a review identified by their unique IGDB ID.
+func (c *Client) GetReview(id int, opts ...OptionFunc) (*Review, error) {
+	opt := newOpt()
+
+	for _, optFunc := range opts {
+		optFunc(&opt)
+	}
+
+	url := c.rootURL + "reviews/" + strconv.Itoa(id)
+	if opts != nil {
+		if values := opt.Values.Encode(); values != "" {
+			url += "?" + values
+		}
+	}
+
+	var r []Review
+
+	err := c.get(url, &r)
+	if err != nil {
+		return nil, err
+	}
+
+	return &r[0], nil
+}
+
+// GetReviews gets IGDB information for a list of reviews identified by a list of their unique IGDB IDs.
+func (c *Client) GetReviews(ids []int, opts ...OptionFunc) ([]*Review, error) {
+	opt := newOpt()
+
+	for _, optFunc := range opts {
+		optFunc(&opt)
+	}
+
+	str := intsToString(ids)
+	url := c.rootURL + "reviews/" + strings.Join(str, ",")
+	if opts != nil {
+		if values := opt.Values.Encode(); values != "" {
+			url += "?" + values
+		}
+	}
+
+	var r []*Review
+
+	err := c.get(url, &r)
+	if err != nil {
+		return nil, err
+	}
+
+	return r, nil
+}
+
+// SearchReviews searches the IGDB using the given query and returns IGDB information
+// for the results. Use functional options for pagination and to sort results by parameter.
+func (c *Client) SearchReviews(qry string, opts ...OptionFunc) ([]*Review, error) {
+	opt := newOpt()
+
+	for _, optFunc := range opts {
+		optFunc(&opt)
+	}
+
+	url := c.rootURL + "reviews/?search=" + qry
+	if opts != nil {
+		if values := opt.Values.Encode(); values != "" {
+			url += "&" + values
+		}
+	}
+
+	var r []*Review
+
+	err := c.get(url, &r)
+	if err != nil {
+		return nil, err
+	}
+
+	return r, nil
 }
