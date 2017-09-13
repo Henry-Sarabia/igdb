128f0b429154a603c5524c55f151c2f3b7510aff
Henry-Sarabia <henrysarabia@mail.fresnostate.edu>
2017-09-12 19:44:26 -0700

    Add Platform fetching functionality

--- platform.go
+++ platform.go
@@ -1,5 +1,10 @@
 package igdb
 
+import (
+	"strconv"
+	"strings"
+)
+
 // RegionCode codes
 type RegionCode int
 
@@ -51,4 +56,79 @@ type Platform struct {
 	Generation int               `json:"generation"`
 	Games      []int             `json:"games"`
 	Version    []PlatformVersion `json:"version"`
+}
+
+// GetPlatform gets IGDB information for a platform identified by its unique IGDB ID.
+func (c *Client) GetPlatform(id int, opts ...OptionFunc) (*Platform, error) {
+	opt := newOpt()
+	for _, optFunc := range opts {
+		optFunc(&opt)
+	}
+
+	url := c.rootURL + "platforms/" + strconv.Itoa(id)
+	if opts != nil {
+		if values := opt.Values.Encode(); values != "" {
+			url += "?" + values
+		}
+	}
+
+	var p []Platform
+
+	err := c.get(url, &p)
+	if err != nil {
+		return nil, err
+	}
+
+	return &p[0], nil
+}
+
+// GetPlatforms gets IGDB information for a list of platforms identified by their
+// unique IGDB IDs.
+func (c *Client) GetPlatforms(ids []int, opts ...OptionFunc) ([]*Platform, error) {
+	opt := newOpt()
+	for _, optFunc := range opts {
+		optFunc(&opt)
+	}
+
+	str := intsToString(ids)
+	url := c.rootURL + "platforms/" + strings.Join(str, ",")
+	if opts != nil {
+		if values := opt.Values.Encode(); values != "" {
+			url += "?" + values
+		}
+	}
+
+	var p []*Platform
+
+	err := c.get(url, &p)
+	if err != nil {
+		return nil, err
+	}
+
+	return p, nil
+}
+
+// SearchPlatforms searches the IGDB using the given query and returns IGDB information
+// for the results. Use functional options for pagination and to sort results by parameter.
+func (c *Client) SearchPlatforms(qry string, opts ...OptionFunc) ([]*Platform, error) {
+	opt := newOpt()
+	for _, optFunc := range opts {
+		optFunc(&opt)
+	}
+
+	url := c.rootURL + "platforms/?search=" + qry
+	if opts != nil {
+		if values := opt.Values.Encode(); values != "" {
+			url += "&" + values
+		}
+	}
+
+	var p []*Platform
+
+	err := c.get(url, &p)
+	if err != nil {
+		return nil, err
+	}
+
+	return p, nil
 }
