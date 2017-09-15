e11780302b76fcf2dfcd27ae894beca7991cb64f
Henry-Sarabia <henrysarabia@mail.fresnostate.edu>
2017-09-15 16:06:40 -0700

    Add missing fields to type Person

--- person.go
+++ person.go
@@ -7,16 +7,21 @@ import (
 
 // Person type
 type Person struct {
-	ID         int    `json:"id"`
-	Name       string `json:"name"`
-	Slug       string `json:"slug"`
-	URL        URL    `json:"url"`
-	CreatedAt  int    `json:"created_at"` //unix epoch
-	UpdatedAt  int    `json:"updated_at"` //unix epoch
-	Mugshot    Image  `json:"mug_shot"`
-	Games      []int  `json:"games"`
-	Characters []int  `json:"characters"`
-	VoiceActed []int  `json:"voice_acted"`
+	ID          int         `json:"id"`
+	Name        string      `json:"name"`
+	Slug        string      `json:"slug"`
+	URL         URL         `json:"url"`
+	CreatedAt   int         `json:"created_at"` //unix epoch
+	UpdatedAt   int         `json:"updated_at"` //unix epoch
+	DOB         int         `json:"dob"`
+	Gender      GenderCode  `json:"gender"`
+	Country     CountryCode `json:"country"`
+	Mugshot     Image       `json:"mug_shot"`
+	Bio         string      `json:"bio"`
+	Description string      `json:"description"`
+	Games       []int       `json:"games"`
+	Characters  []int       `json:"characters"`
+	VoiceActed  []int       `json:"voice_acted"`
 }
 
 // GetPerson gets IGDB information for a person identified by its unique IGDB ID.
