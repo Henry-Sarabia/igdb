6cb67115adc19dfaa19e97448b2308e94a20f97e
Henry-Sarabia <henrysarabia@mail.fresnostate.edu>
2017-09-26 16:09:13 -0700

    Add multiURL test

--- igdb_test.go
+++ igdb_test.go
@@ -21,3 +21,18 @@ func TestSingleURL(t *testing.T) {
 		t.Errorf("Expected URL '%s', got '%s'", eURL, aURL)
 	}
 }
+
+func TestMultiURL(t *testing.T) {
+	c := NewClient()
+
+	eURL := "https://api-2445582011268.apicast.io/tests/1234,5678,9?fields=id%2Cname%2Cpopularity&filter%5Bpopularity%5D%5Bgte%5D=50&limit=10&offset=5&order=popularity%3Adesc"
+	aURL := c.multiURL(testEndpoint, []int{1234, 5678, 9},
+		OptFields("id", "name", "popularity"),
+		OptFilter("popularity", GTE, strconv.Itoa(50)),
+		OptLimit(10),
+		OptOffset(5),
+		OptOrder("popularity", Desc))
+	if aURL != eURL {
+		t.Errorf("Expected URL '%s', got '%s'", eURL, aURL)
+	}
+}
