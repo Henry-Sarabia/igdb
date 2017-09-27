a5a81583a56fb9e4c9c64132ff8f864b443b3064
Henry-Sarabia <henrysarabia@mail.fresnostate.edu>
2017-09-26 17:07:33 -0700

    Add OptLimit test

--- options_test.go
+++ options_test.go
@@ -1,6 +1,9 @@
 package igdb
 
-import "testing"
+import (
+	"strconv"
+	"testing"
+)
 
 func TestOptOrder(t *testing.T) {
 	opt := newOpt()
@@ -12,5 +15,21 @@ func TestOptOrder(t *testing.T) {
 	aOrd := opt.Values.Get("order")
 	if aOrd != eOrd {
 		t.Errorf("Expected order '%s', got '%s'", eOrd, aOrd)
+	}
+}
+
+func TestOptLimit(t *testing.T) {
+	opt := newOpt()
+	optFunc := OptLimit(20)
+
+	optFunc(&opt)
+
+	eLim := 20
+	aLim, err := strconv.Atoi(opt.Values.Get("limit"))
+	if err != nil {
+		t.Error(err)
+	}
+	if aLim != eLim {
+		t.Errorf("Expected limit %d, got %d", eLim, aLim)
 	}
 }
