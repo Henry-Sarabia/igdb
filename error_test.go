8529e01d04ea24a17e97500e045fa214150c2f5f
Henry-Sarabia <henrysarabia@mail.fresnostate.edu>
2017-09-27 23:17:32 -0700

    Add additional checkError testing

--- error_test.go
+++ error_test.go
@@ -1,7 +1,11 @@
 package igdb
 
-import "testing"
-import "net/http"
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+	"testing"
+)
 
 const checkErrorResp = `
 {
@@ -24,5 +28,32 @@ func TestCheckError(t *testing.T) {
 	actErr := err.Error()
 	if actErr != expErr {
 		t.Errorf("Expected error '%s', got '%s'", expErr, actErr)
+	}
+}
+
+func TestCheckErrorResp(t *testing.T) {
+	var errTests = []struct {
+		Name   string
+		Status string
+		Code   int
+		Body   string
+		Exp    string
+	}{
+		{"empty response", "404 Not Found", 404, "", "unexpected end of JSON input"},
+	}
+
+	for _, et := range errTests {
+		t.Run(et.Name, func(t *testing.T) {
+			c := NewClient()
+
+			resp := &http.Response{Status: et.Status,
+				StatusCode: et.Code,
+				Body:       ioutil.NopCloser(bytes.NewBufferString(et.Body)),
+			}
+			err := c.checkError(resp)
+			if err.Error() != et.Exp {
+				t.Errorf("Expected '%v', got '%v'", et.Exp, err.Error())
+			}
+		})
 	}
 }
