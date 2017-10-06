0876b78cfdc0d8c9737e8c699ae4bc0038f09d73
Henry-Sarabia <henrysarabia@mail.fresnostate.edu>
2017-10-05 23:29:10 -0700

    Refactor all option tests to check for errors and use table driven testing

--- options_test.go
+++ options_test.go
@@ -1,185 +1,301 @@
 package igdb
 
 import (
-	"strconv"
+	"fmt"
+	"reflect"
 	"testing"
 )
 
-func TestOptOrder(t *testing.T) {
-	opt, err := newOpt()
-	if err != nil {
-		t.Error(err)
+func TestNewOpt(t *testing.T) {
+	var optTests = []struct {
+		Name     string
+		OptFuncs []OptionFunc
+		ExpCount int
+		ExpErr   error
+	}{
+		{"Empty option", []OptionFunc{}, 0, nil},
+		{"Single option", []OptionFunc{OptLimit(4)}, 1, nil},
+		{"Multiple options", []OptionFunc{OptOffset(10), OptLimit(50), OptFields("id", "rating"), OptFilter("rating", OpLessThan, "40"), OptOrder("rating", AscendingOrder)}, 5, nil},
+		{"Multiple filter options", []OptionFunc{OptFilter("popularity", OpLessThan, "50"), OptFilter("rating", OpGreaterThan, "50")}, 2, nil},
 	}
-	optFunc := OptOrder("popularity", Ascend)
 
-	optFunc(opt)
+	for _, ot := range optTests {
+		t.Run(ot.Name, func(t *testing.T) {
+			opt, err := newOpt(ot.OptFuncs...)
+			if !reflect.DeepEqual(err, ot.ExpErr) {
+				t.Fatalf("Expected error '%v', got '%v'", ot.ExpErr, err)
+			}
 
-	eOrd := "popularity:asc"
-	aOrd := opt.Values.Get("order")
-	if aOrd != eOrd {
-		t.Errorf("Expected order '%s', got '%s'", eOrd, aOrd)
+			actCount := len(opt.Values)
+			if actCount != ot.ExpCount {
+				t.Fatalf("Expected count of %d, got %d", ot.ExpCount, actCount)
+			}
+		})
+	}
+}
+
+func TestOptOrder(t *testing.T) {
+	var orderTests = []struct {
+		Name   string
+		Field  string
+		Order  order
+		ExpOrd string
+		ExpErr error
+	}{
+		{"Non-empty field", "popularity", AscendingOrder, "popularity:asc", nil},
+		{"Empty field", "", DescendingOrder, "", ErrEmptyField},
+	}
+
+	for _, ot := range orderTests {
+		t.Run(ot.Name, func(t *testing.T) {
+			opt, err := newOpt()
+			if err != nil {
+				t.Fatalf(err.Error())
+			}
+			optFunc := OptOrder(ot.Field, ot.Order)
+
+			err = optFunc(opt)
+			if !reflect.DeepEqual(err, ot.ExpErr) {
+				t.Fatalf("Expected error '%v', got '%v'", ot.ExpErr, err)
+			}
+
+			actOrd := opt.Values.Get("order")
+			if actOrd != ot.ExpOrd {
+				t.Fatalf("Expected order '%s', got '%s'", ot.ExpOrd, actOrd)
+			}
+		})
 	}
 }
 
 func TestOptLimit(t *testing.T) {
-	opt, err := newOpt()
-	if err != nil {
-		t.Error(err)
+	var limitTests = []struct {
+		Name   string
+		Limit  int
+		ExpLim string
+		ExpErr error
+	}{
+		{"Limit within range", 15, "15", nil},
+		{"Zero limit", 0, "", ErrOutOfRange},
+		{"Limit below range", -10, "", ErrOutOfRange},
+		{"Limit above range", 51, "", ErrOutOfRange},
 	}
-	optFunc := OptLimit(20)
-	// var limitTests = []struct {
-	// 	Name string
-	// 	Limit int
-	// } {
-	// 	{"Happy path", 20},
-	// 	{"invalid negative limit", }
-	// }
 
-	optFunc(opt)
+	for _, lt := range limitTests {
+		t.Run(lt.Name, func(t *testing.T) {
+			opt, err := newOpt()
+			if err != nil {
+				t.Fatalf(err.Error())
+			}
+			optFunc := OptLimit(lt.Limit)
 
-	eLim := strconv.Itoa(20)
-	aLim := opt.Values.Get("limit")
+			err = optFunc(opt)
+			if !reflect.DeepEqual(err, lt.ExpErr) {
+				t.Fatalf("Expected error '%v', got '%v'", lt.ExpErr, err)
+			}
 
-	if aLim != eLim {
-		t.Errorf("Expected limit %s, got %s", eLim, aLim)
+			actLim := opt.Values.Get("limit")
+			if actLim != lt.ExpLim {
+				t.Fatalf("Expected limit '%s', got '%s'", lt.ExpLim, actLim)
+			}
+		})
 	}
 }
 
 func TestOptOffset(t *testing.T) {
-	opt, err := newOpt()
-	if err != nil {
-		t.Error(err)
+	var offsetTests = []struct {
+		Name   string
+		Offset int
+		ExpOff string
+		ExpErr error
+	}{
+		{"Offset within range", 20, "20", nil},
+		{"Zero offset", 0, "0", nil},
+		{"Offset below range", -15, "", ErrOutOfRange},
+		{"Offset above range", 100, "", ErrOutOfRange},
 	}
-	optFunc := OptOffset(5)
 
-	optFunc(opt)
+	for _, ot := range offsetTests {
+		t.Run(ot.Name, func(t *testing.T) {
+			opt, err := newOpt()
+			if err != nil {
+				t.Fatalf(err.Error())
+			}
+			optFunc := OptOffset(ot.Offset)
 
-	eOff := strconv.Itoa(5)
-	aOff := opt.Values.Get("offset")
-	if aOff != eOff {
-		t.Errorf("Expected offset %s, got %s", eOff, aOff)
+			err = optFunc(opt)
+			if !reflect.DeepEqual(err, ot.ExpErr) {
+				t.Fatalf("Expected error '%v', got '%v'", ot.ExpErr, err)
+			}
+
+			actOff := opt.Values.Get("offset")
+			if actOff != ot.ExpOff {
+				t.Fatalf("Expected offset '%s', got '%s'", ot.ExpOff, actOff)
+			}
+		})
 	}
 }
 
 func TestOptFields(t *testing.T) {
-	opt, err := newOpt()
-	if err != nil {
-		t.Error(err)
+	var fieldsTests = []struct {
+		Name      string
+		Fields    []string
+		ExpFields string
+		ExpErr    error
+	}{
+		{"Single non-empty field", []string{"name"}, "name", nil},
+		{"Multiple non-empty fields", []string{"name", "popularity", "rating"}, "name,popularity,rating", nil},
+		{"Empty fields slice", []string{}, "", ErrEmptySlice},
+		{"Single empty field", []string{""}, "", ErrEmptyField},
+		{"Multiple empty fields", []string{"", "", "", ""}, "", ErrEmptyField},
+		{"Mixed empty and non-empty fields", []string{"", "id", "", "url"}, "", ErrEmptyField},
 	}
-	optFunc := OptFields("name", "rating", "popularity")
 
-	optFunc(opt)
+	for _, ft := range fieldsTests {
+		t.Run(ft.Name, func(t *testing.T) {
+			opt, err := newOpt()
+			if err != nil {
+				t.Fatalf(err.Error())
+			}
+			optFunc := OptFields(ft.Fields...)
 
-	eFld := "name,rating,popularity"
-	aFld := opt.Values.Get("fields")
-	if aFld != eFld {
-		t.Errorf("Expected fields '%s', got '%s'", eFld, aFld)
-	}
-}
+			err = optFunc(opt)
+			if !reflect.DeepEqual(err, ft.ExpErr) {
+				t.Fatalf("Expected error '%v', got '%v'", ft.ExpErr, err)
+			}
 
-func TestOptFieldsEmpty(t *testing.T) {
-	opt, err := newOpt()
-	if err != nil {
-		t.Error(err)
-	}
-	optFunc := OptFields()
-
-	optFunc(opt)
-
-	eFld := ""
-	aFld := opt.Values.Get("fields")
-	if aFld != eFld {
-		t.Errorf("Expected empty fields, got '%s'", aFld)
+			actFields := opt.Values.Get("fields")
+			if actFields != ft.ExpFields {
+				t.Fatalf("Expected order '%s', got '%s'", ft.ExpFields, actFields)
+			}
+		})
 	}
 }
 
 func TestOptFilter(t *testing.T) {
-	opt, err := newOpt()
-	if err != nil {
-		t.Error(err)
+	var filterTests = []struct {
+		Name      string
+		Field     string
+		Op        operator
+		Val       string
+		ExpFilter string
+		ExpErr    error
+	}{
+		{"Non-empty field and non-empty value", "rating", OpGreaterThanEqual, "60", "", nil},
+		{"Non-empty field and empty value", "name", OpPrefix, "", "", ErrEmptyField},
+		{"Empty field and non-empty value", "", OpEquals, "Megaman X1", "", ErrEmptyField},
+		{"Empty field and empty value", "", OpIn, "", "", ErrEmptyField},
 	}
-	optFunc := OptFilter("popularity", LessThanEqual, "50")
 
-	optFunc(opt)
+	for _, ft := range filterTests {
+		t.Run(ft.Name, func(t *testing.T) {
+			opt, err := newOpt()
+			if err != nil {
+				t.Fatalf(err.Error())
+			}
+			optFunc := OptFilter(ft.Field, ft.Op, ft.Val)
 
-	eFil := "50"
-	aFil := opt.Values.Get("filter[popularity][lte]")
-	if aFil != eFil {
-		t.Errorf("Expected filter '%s', got '%s'", eFil, aFil)
-	}
-}
+			err = optFunc(opt)
+			if !reflect.DeepEqual(err, ft.ExpErr) {
+				t.Fatalf("Expected error '%v', got '%v'", ft.ExpErr, err)
+			}
 
-func TestOptSearch(t *testing.T) {
-	opt, err := newOpt()
-	if err != nil {
-		t.Error(err)
-	}
-	optFunc := optSearch("mario party")
-
-	optFunc(opt)
-
-	eQry := "mario party"
-	aQry := opt.Values.Get("search")
-	if aQry != eQry {
-		t.Errorf("Expected query '%s', got '%s'", eQry, aQry)
+			actFilter := opt.Values.Get(fmt.Sprintf("[%s][%s]", ft.Field, ft.Op))
+			if actFilter != ft.ExpFilter {
+				t.Fatalf("Expected order '%s', got '%s'", ft.ExpFilter, actFilter)
+			}
+		})
 	}
 }
 
 func TestOptScroll(t *testing.T) {
-	opt, err := newOpt()
-	if err != nil {
-		t.Error(err)
-	}
-	optFunc := OptScroll(3)
-
-	optFunc(opt)
-
-	ePage := strconv.Itoa(3)
-	aPage := opt.Values.Get("scroll")
-	if aPage != ePage {
-		t.Errorf("Expected page %s, got %s", ePage, aPage)
-	}
-}
-
-func TestNewOptEmpty(t *testing.T) {
-	opt, err := newOpt()
-	if err != nil {
-		t.Error(err)
+	var scrollTests = []struct {
+		Name    string
+		Page    int
+		ExpPage string
+		ExpErr  error
+	}{
+		{"Page within range", 5, "5", nil},
+		{"Zero page", 0, "", ErrOutOfRange},
+		{"Page below range", -15, "", ErrOutOfRange},
 	}
 
-	evl := 0
-	avl := len(opt.Values)
-	if avl != evl {
-		t.Errorf("Expected Values map length %d, got %d", evl, avl)
-	}
-}
+	for _, st := range scrollTests {
+		t.Run(st.Name, func(t *testing.T) {
+			opt, err := newOpt()
+			if err != nil {
+				t.Fatalf(err.Error())
+			}
+			optFunc := OptScroll(st.Page)
 
-func TestNewOptSingle(t *testing.T) {
-	opt, err := newOpt(OptLimit(20))
-	if err != nil {
-		t.Error(err)
-	}
+			err = optFunc(opt)
+			if !reflect.DeepEqual(err, st.ExpErr) {
+				t.Fatalf("Expected error '%v', got '%v'", st.ExpErr, err)
+			}
 
-	evl := 1
-	avl := len(opt.Values)
-	if avl != evl {
-		t.Errorf("Expected Values map length %d, got %d", evl, avl)
+			actPage := opt.Values.Get("scroll")
+			if actPage != st.ExpPage {
+				t.Fatalf("Expected offset '%s', got '%s'", st.ExpPage, actPage)
+			}
+		})
 	}
 }
 
-func TestNewOptMulti(t *testing.T) {
-	opt, err := newOpt(OptFields("name", "rating"),
-		OptFilter("name", Equals, "zelda"),
-		OptLimit(5),
-		OptOffset(10),
-		OptOrder("rating", Descend))
-	if err != nil {
-		t.Error(err)
+func TestOptSearch(t *testing.T) {
+	var searchTests = []struct {
+		Name   string
+		Qry    string
+		ExpQry string
+		ExpErr error
+	}{
+		{"Non-empty query", "zelda", "zelda", nil},
+		{"Non-Empty query with spaces", "the legend of zelda", "the legend of zelda", nil},
+		{"Empty query", "", "", ErrEmptyQuery},
 	}
 
-	evl := 5
-	avl := len(opt.Values)
-	if avl != evl {
-		t.Errorf("Expected Values map length %d, got %d", evl, avl)
+	for _, st := range searchTests {
+		t.Run(st.Name, func(t *testing.T) {
+			opt, err := newOpt()
+			if err != nil {
+				t.Fatalf(err.Error())
+			}
+			optFunc := optSearch(st.Qry)
+
+			err = optFunc(opt)
+			if !reflect.DeepEqual(err, st.ExpErr) {
+				t.Fatalf("Expected error '%v', got '%v'", st.ExpErr, err)
+			}
+
+			actQry := opt.Values.Get("search")
+			if actQry != st.ExpQry {
+				t.Fatalf("Expected offset '%s', got '%s'", st.ExpQry, actQry)
+			}
+		})
+	}
+}
+
+func TestOptOverlap(t *testing.T) {
+	var overlapTests = []struct {
+		Name     string
+		OptFuncs []OptionFunc
+		ExpErr   error
+	}{
+		{"OptOrder overlap", []OptionFunc{OptOrder("popularity", DescendingOrder), OptOrder("rating", AscendingOrder)}, ErrOptionSet},
+		{"OptLimit overlap", []OptionFunc{OptLimit(5), OptLimit(40)}, ErrOptionSet},
+		{"OptOffset overlap", []OptionFunc{OptOffset(0), OptOffset(25)}, ErrOptionSet},
+		{"OptFields overlap", []OptionFunc{OptFields("id"), OptFields("name")}, ErrOptionSet},
+		{"OptFilter overlap", []OptionFunc{OptFilter("rating", OpLessThan, "50"), OptFilter("popularity", OpGreaterThan, "50")}, nil},
+		{"OptScroll overlap", []OptionFunc{OptScroll(1), OptScroll(10)}, ErrOptionSet},
+		{"OptSearch overlap", []OptionFunc{optSearch("zelda"), optSearch("link")}, ErrOptionSet},
+		{"OptOffset and OptScroll overlap", []OptionFunc{OptOffset(40), OptScroll(4)}, ErrExclusiveOption},
+		{"OptScroll and OptOffset overlap", []OptionFunc{OptScroll(3), OptOffset(30)}, ErrExclusiveOption},
+	}
+
+	for _, ot := range overlapTests {
+		t.Run(ot.Name, func(t *testing.T) {
+			_, err := newOpt(ot.OptFuncs...)
+			if !reflect.DeepEqual(err, ot.ExpErr) {
+				t.Fatalf("Expected error '%v', got '%v'", ot.ExpErr, err)
+			}
+		})
 	}
 }
