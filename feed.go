8eeba8f9e04b4290491a9fa4d3678a94c8d468d1
Henry-Sarabia <henrysarabia@mail.fresnostate.edu>
2017-10-10 18:34:03 -0700

    Add documentation and Stringer method to FeedCategory

--- feed.go
+++ feed.go
@@ -1,6 +1,10 @@
 package igdb
 
-// FeedCategory codes
+// FeedCategory corresponds to the IGDB
+// enumerated feed item category which
+// describes the type of feed item in
+// a particular feed. FeedCategory
+// implements the Stringer interface.
 type FeedCategory int
 
 // Feed is
@@ -57,4 +61,33 @@ func (c *Client) GetFeeds(ids []int, opts ...OptionFunc) ([]*Feed, error) {
 	}
 
 	return f, nil
+}
+
+// FeedCategory's string method matches the code with
+//  IGDB's Feed Item Category enumerated type
+// and return the category as a string. Codes with
+// no match will returned "Undefined".
+
+// FeedCategory implements the Stringer interface
+// by matching its code with the IGDBs enumerated
+// type Feed Item Category and returns the category
+// as a string. Codes with no match will return
+// "Undefined".
+func (f FeedCategory) String() string {
+	switch f {
+	case 1:
+		return "Pulse Article"
+	case 2:
+		return "Coming Soon"
+	case 3:
+		return "New Trailer"
+	case 5:
+		return "User Contributed Item"
+	case 6:
+		return "User Contributions Item"
+	case 7:
+		return "Page Contributed Item"
+	default:
+		return "Undefined"
+	}
 }
