ae1663cf786512da1994ba532ce7cb394fb74f5f
Henry-Sarabia <henrysarabia@mail.fresnostate.edu>
2017-10-16 20:30:32 -0700

    Update Keyword documentation

--- keyword.go
+++ keyword.go
@@ -1,6 +1,9 @@
 package igdb
 
-// Keyword type
+// Keyword contains information on an IGDB
+// entry for a particular keyword. Keywords
+// are words or phrases that get tagged to
+// a game (e.g. "world war 2" or "steampunk").
 type Keyword struct {
 	ID        int    `json:"id"`
 	Name      string `json:"name"`
@@ -11,7 +14,11 @@ type Keyword struct {
 	Games     []int  `json:"games"`
 }
 
-// GetKeyword gets IGDB information for a keyword identified by its unique IGDB ID.
+// GetKeyword gets IGDB information for a Keyword identified by its unique
+// IGDB ID. GetKeyword returns a single Keyword identified by the provided
+// IGDB ID. Functional options may be provided but sorting and pagination
+// will not have an effect due to GetKeyword only returning a single Keyword
+// object and not a list of Keywords.
 func (c *Client) GetKeyword(id int, opts ...OptionFunc) (*Keyword, error) {
 	url, err := c.singleURL(KeywordEndpoint, id, opts...)
 	if err != nil {
@@ -27,8 +34,10 @@ func (c *Client) GetKeyword(id int, opts ...OptionFunc) (*Keyword, error) {
 	return &k[0], nil
 }
 
-// GetKeywords gets IGDB information for a list of keywords identified by their
-// unique IGDB IDs.
+// GetKeywords returns a list of Keywords identified by the provided list of IGDB
+// IDs. Provide functional options to filter, sort, and paginate the results.
+// Providing an empty list of IDs will instead retrieve an index of Keywords based
+// solely on the provided options.
 func (c *Client) GetKeywords(ids []int, opts ...OptionFunc) ([]*Keyword, error) {
 	url, err := c.multiURL(KeywordEndpoint, ids, opts...)
 	if err != nil {
@@ -44,8 +53,8 @@ func (c *Client) GetKeywords(ids []int, opts ...OptionFunc) ([]*Keyword, error) 
 	return k, nil
 }
 
-// SearchKeywords searches the IGDB using the given query and returns IGDB information
-// for the results. Use functional options for pagination and to sort results by parameter.
+// SearchKeywords returns a list of Keywords found by searching the IGDB using the
+// provided query. Provide functional options to filter, sort, and paginate the results.
 func (c *Client) SearchKeywords(qry string, opts ...OptionFunc) ([]*Keyword, error) {
 	url, err := c.searchURL(KeywordEndpoint, qry, opts...)
 	if err != nil {
