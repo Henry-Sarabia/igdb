454ed0134f4d4eff1a3f3bd9c1c15b2d2aa4631c
Henry-Sarabia <henrysarabia@mail.fresnostate.edu>
2017-10-20 01:17:37 -0700

    Update PulseGroup and PulseSource documentation

--- pulsegroup.go
+++ pulsegroup.go
@@ -1,6 +1,7 @@
 package igdb
 
-// PulseGroup type
+// PulseGroup contains information on an
+// IGDB entry for a group of news articles.
 type PulseGroup struct {
 	ID          int    `json:"id"`
 	Name        string `json:"name"`
@@ -15,7 +16,11 @@ type PulseGroup struct {
 	Game        int    `json:"game"`
 }
 
-// GetPulseGroup gets IGDB information for a pulse group identified by its unique IGDB ID.
+// GetPulseGroup gets IGDB information for a PulseGroup identified by its unique
+// IGDB ID. GetPulseGroup returns a single PulseGroup identified by the provided
+// IGDB ID. Functional options may be provided but sorting and pagination will
+// not have an effect due to GetPulseGroup only returning a single PulseGroup
+// object and not a list of PulseGroups.
 func (c *Client) GetPulseGroup(id int, opts ...OptionFunc) (*PulseGroup, error) {
 	url, err := c.singleURL(PulseGroupEndpoint, id, opts...)
 	if err != nil {
@@ -31,8 +36,10 @@ func (c *Client) GetPulseGroup(id int, opts ...OptionFunc) (*PulseGroup, error) 
 	return &p[0], nil
 }
 
-// GetPulseGroups gets IGDB information for a list of pulse groups identified by their
-// unique IGDB IDs.
+// GetPulseGroups returns a list of PulseGroups identified by the provided list of
+// IGDB IDs. Provide functional options to filter, sort, and paginate the results.
+// Providing an empty list of IDs will instead retrieve an index of PulseGroups based
+// solely on the provided options.
 func (c *Client) GetPulseGroups(ids []int, opts ...OptionFunc) ([]*PulseGroup, error) {
 	url, err := c.multiURL(PulseGroupEndpoint, ids, opts...)
 	if err != nil {
@@ -48,8 +55,8 @@ func (c *Client) GetPulseGroups(ids []int, opts ...OptionFunc) ([]*PulseGroup, e
 	return p, nil
 }
 
-// SearchPulseGroups searches the IGDB using the given query and returns IGDB information
-// for the results. Use functional options for pagination and to sort results by parameter.
+// SearchPulseGroups returns a list of PulseGroups found by searching the IGDB using the
+// provided query. Provide functional options to filter, sort, and paginate the results.
 func (c *Client) SearchPulseGroups(qry string, opts ...OptionFunc) ([]*PulseGroup, error) {
 	url, err := c.searchURL(PulseGroupEndpoint, qry, opts...)
 	if err != nil {
--- pulsesource.go
+++ pulsesource.go
@@ -1,6 +1,8 @@
 package igdb
 
-// PulseSource type
+// PulseSource contains information
+// on an IGDB entry for a specific
+// news source.
 type PulseSource struct {
 	ID   int    `json:"id"`
 	Name string `json:"name"`
@@ -8,7 +10,11 @@ type PulseSource struct {
 	Page int    `json:"page"`
 }
 
-// GetPulseSource gets IGDB information for a pulse source identified by its unique IGDB ID.
+// GetPulseSource gets IGDB information for a PulseSource identified by its unique
+// IGDB ID. GetPulseSource returns a single PulseSource identified by the provided
+// IGDB ID. Functional options may be provided but sorting and pagination will not
+// have an effect due to GetPulseSource only returning a single PulseSource object
+// and not a list of PulseSources.
 func (c *Client) GetPulseSource(id int, opts ...OptionFunc) (*PulseSource, error) {
 	url, err := c.singleURL(PulseSourceEndpoint, id, opts...)
 	if err != nil {
@@ -24,8 +30,10 @@ func (c *Client) GetPulseSource(id int, opts ...OptionFunc) (*PulseSource, error
 	return &p[0], nil
 }
 
-// GetPulseSources gets IGDB information for a list of pulse sources identified by their
-// unique IGDB IDs.
+// GetPulseSources returns a list of PulseSources identified by the provided list of
+// IGDB IDs. Provide functional options to filter, sort, and paginate the results.
+// Providing an empty list of IDs will instead retrieve an index of PulseSources based
+// solely on the provided options.
 func (c *Client) GetPulseSources(ids []int, opts ...OptionFunc) ([]*PulseSource, error) {
 	url, err := c.multiURL(PulseSourceEndpoint, ids, opts...)
 	if err != nil {
@@ -41,8 +49,8 @@ func (c *Client) GetPulseSources(ids []int, opts ...OptionFunc) ([]*PulseSource,
 	return p, nil
 }
 
-// SearchPulseSources searches the IGDB using the given query and returns IGDB information
-// for the results. Use functional options for pagination and to sort results by parameter.
+// SearchPulseSources returns a list of PulseSources found by searching the IGDB using the
+// provided query. Provide functional options to filter, sort, and paginate the results.
 func (c *Client) SearchPulseSources(qry string, opts ...OptionFunc) ([]*PulseSource, error) {
 	url, err := c.searchURL(PulseSourceEndpoint, qry, opts...)
 	if err != nil {
