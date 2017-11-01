62e8c1c90ad1bdf47a2479114cb136f086fa3797
Henry-Sarabia <henrysarabia@mail.fresnostate.edu>
2017-11-01 16:21:26 -0700

    Move Title test data to test_data folder

--- title_test.go
+++ title_test.go
@@ -6,85 +6,6 @@ import (
 	"testing"
 )
 
-const getTitleResp = `
-[{
-	"id": 16549,
-	"name": "Dev. Support Engineer",
-	"created_at": 1433110945497,
-	"updated_at": 1433110945600,
-	"slug": "dev-support-engineer",
-	"url": "https://www.igdb.com/titles/dev-support-engineer",
-	"games": [
-		1000
-	]
-}]
-`
-
-const getTitlesResp = `
-[{
-	"id": 7839,
-	"name": "Web Dev",
-	"created_at": 1413999800195,
-	"updated_at": 1413999800195,
-	"slug": "web-dev",
-	"url": "https://www.igdb.com/titles/web-dev",
-	"games": [
-		4754
-	]
-},
-{
-	"id": 25381,
-	"name": "Embedded QA",
-	"created_at": 1461934414039,
-	"updated_at": 1472328870944,
-	"slug": "embedded-qa",
-	"url": "https://www.igdb.com/titles/embedded-qa",
-	"games": [
-		556,
-		15894,
-		105,
-		11582
-	]
-}]
-`
-
-const searchTitlesResp = `
-[{
-	"id": 17228,
-	"name": "Senior Graphic & Interface Designer",
-	"created_at": 1437825411943,
-	"updated_at": 1437825412024,
-	"slug": "senior-graphic-and-interface-designer",
-	"url": "https://www.igdb.com/titles/senior-graphic-and-interface-designer",
-	"games": [
-		2995
-	]
-},
-{
-	"id": 16396,
-	"name": "Graphic Coordinator",
-	"created_at": 1433023246973,
-	"updated_at": 1479009213820,
-	"slug": "graphic-coordinator",
-	"url": "https://www.igdb.com/titles/graphic-coordinator",
-	"games": [
-		3824,
-		5418
-	]
-},
-{
-	"id": 16514,
-	"name": "Character Graphic",
-	"created_at": 1433023452820,
-	"updated_at": 1433023454932,
-	"slug": "character-graphic",
-	"url": "https://www.igdb.com/titles/character-graphic",
-	"games": [
-		9141
-	]
-}]
-`
-
 func TestTitleTypeIntegrity(t *testing.T) {
 	if testing.Short() {
 		t.Skip("Skipping test requiring communication with external server")
@@ -102,7 +23,10 @@ func TestTitleTypeIntegrity(t *testing.T) {
 }
 
 func TestGetTitle(t *testing.T) {
-	ts, c := testServerString(http.StatusOK, getTitleResp)
+	ts, c, err := testServerFile(http.StatusOK, "test_data/get_title.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
 	defer ts.Close()
 
 	ti, err := c.GetTitle(16549)
@@ -138,7 +62,10 @@ func TestGetTitle(t *testing.T) {
 }
 
 func TestGetTitles(t *testing.T) {
-	ts, c := testServerString(http.StatusOK, getTitlesResp)
+	ts, c, err := testServerFile(http.StatusOK, "test_data/get_titles.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
 	defer ts.Close()
 
 	ids := []int{7839, 25381}
@@ -181,7 +108,10 @@ func TestGetTitles(t *testing.T) {
 }
 
 func TestSearchTitles(t *testing.T) {
-	ts, c := testServerString(http.StatusOK, searchTitlesResp)
+	ts, c, err := testServerFile(http.StatusOK, "test_data/search_titles.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
 	defer ts.Close()
 
 	ti, err := c.SearchTitles("graphic")
