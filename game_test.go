afdc6fac4f329df0cceabc4fa2dcecdad9f0bad8
Henry-Sarabia <henrysarabia@mail.fresnostate.edu>
2017-11-03 23:37:42 -0700

    Add additional Game tests

--- game_test.go
+++ game_test.go
@@ -23,149 +23,201 @@ func TestGameTypeIntegrity(t *testing.T) {
 }
 
 func TestGetGame(t *testing.T) {
-	ts, c, err := testServerFile(http.StatusOK, "test_data/get_game.txt")
-	if err != nil {
-		t.Fatal(err)
+	var gameTests = []struct {
+		Name   string
+		Resp   string
+		ID     int
+		ExpErr string
+	}{
+		{"Happy path", "test_data/get_game.txt", 7346, ""},
+		{"Invalid ID", "test_data/empty.txt", -1, ErrNegativeID.Error()},
+		{"Empty Response", "test_data/empty.txt", 7346, errEndOfJSON.Error()},
 	}
-	defer ts.Close()
+	for _, tt := range gameTests {
+		t.Run(tt.Name, func(t *testing.T) {
+			ts, c, err := testServerFile(http.StatusOK, tt.Resp)
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer ts.Close()
 
-	g, err := c.GetGame(7346)
-	if err != nil {
-		t.Error(err)
-	}
+			g, err := c.GetGame(tt.ID)
+			assertError(t, err, tt.ExpErr)
 
-	eID := 7346
-	aID := g.ID
-	if aID != eID {
-		t.Errorf("Expected ID %d, got %d", eID, aID)
-	}
+			if tt.ExpErr != "" {
+				return
+			}
 
-	en := "The Legend of Zelda: Breath of the Wild"
-	an := g.Name
-	if an != en {
-		t.Errorf("Expected name '%s', got '%s'\n", en, an)
-	}
+			eID := 7346
+			aID := g.ID
+			if aID != eID {
+				t.Errorf("Expected ID %d, got %d", eID, aID)
+			}
 
-	er := 98.5
-	ar := g.AggregateRating
-	if ar != er {
-		t.Errorf("Expected rating of %f, got %f", er, ar)
-	}
+			en := "The Legend of Zelda: Breath of the Wild"
+			an := g.Name
+			if an != en {
+				t.Errorf("Expected name '%s', got '%s'\n", en, an)
+			}
 
-	ew := 2709
-	aw := g.Covers.Width
-	if aw != ew {
-		t.Errorf("Expected width of %d, got %d\n", ew, aw)
-	}
+			er := 98.5
+			ar := g.AggregateRating
+			if ar != er {
+				t.Errorf("Expected rating of %f, got %f", er, ar)
+			}
 
-	var ev = []YoutubeVideo{
-		{"Trailer", "Z6BeAtdoELY"},
-		{"Trailer", "1rPxiXXxftE"},
-		{"Trailer", "vDFZIUdo764"},
-		{"Trailer", "zw47_q9wbBE"}}
-	av := g.Videos
-	for i := range av {
-		if av[i] != ev[i] {
-			t.Errorf("Expected video %v, got video %v\n", ev[i], av[i])
-		}
+			ew := 2709
+			aw := g.Covers.Width
+			if aw != ew {
+				t.Errorf("Expected width of %d, got %d\n", ew, aw)
+			}
+
+			var ev = []YoutubeVideo{
+				{"Trailer", "Z6BeAtdoELY"},
+				{"Trailer", "1rPxiXXxftE"},
+				{"Trailer", "vDFZIUdo764"},
+				{"Trailer", "zw47_q9wbBE"}}
+			av := g.Videos
+			for i := range av {
+				if av[i] != ev[i] {
+					t.Errorf("Expected video %v, got video %v\n", ev[i], av[i])
+				}
+			}
+		})
 	}
 }
 
 func TestGetGames(t *testing.T) {
-	ts, c, err := testServerFile(http.StatusOK, "test_data/get_games.txt")
-	if err != nil {
-		t.Fatal(err)
+	var gameTests = []struct {
+		Name   string
+		Resp   string
+		IDs    []int
+		Opts   []OptionFunc
+		ExpErr string
+	}{
+		{"Happy path", "test_data/get_games.txt", []int{1721, 2777}, []OptionFunc{OptLimit(5)}, ""},
+		{"Invalid ID", "test_data/empty.txt", []int{-500}, nil, ErrNegativeID.Error()},
+		{"Zero IDs", "test_data/empty.txt", nil, nil, ErrEmptyIDs.Error()},
+		{"Empty Response", "test_data/empty.txt", []int{1721, 2777}, nil, errEndOfJSON.Error()},
+		{"Invalid option", "test_data/empty.txt", []int{1721, 2777}, []OptionFunc{OptOffset(9999)}, ErrOutOfRange.Error()},
 	}
-	defer ts.Close()
+	for _, tt := range gameTests {
+		t.Run(tt.Name, func(t *testing.T) {
+			ts, c, err := testServerFile(http.StatusOK, tt.Resp)
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer ts.Close()
 
-	ids := []int{1721, 2777}
-	g, err := c.GetGames(ids)
-	if err != nil {
-		t.Error(err)
-	}
+			g, err := c.GetGames(tt.IDs, tt.Opts...)
+			assertError(t, err, tt.ExpErr)
 
-	el := 2
-	al := len(g)
-	if al != el {
-		t.Errorf("Expected length of %d, got %d", el, al)
-	}
+			if tt.ExpErr != "" {
+				return
+			}
 
-	eURL := URL("https://www.igdb.com/games/mega-man-8")
-	aURL := g[0].URL
-	if aURL != eURL {
-		t.Errorf("Expected URL '%s', got '%s'", eURL, aURL)
-	}
+			el := 2
+			al := len(g)
+			if al != el {
+				t.Errorf("Expected length of %d, got %d", el, al)
+			}
 
-	ec := 1352059102919
-	ac := g[0].CreatedAt
-	if ac != ec {
-		t.Errorf("Expected Unix time in milliseconds of %d, got %d", ec, ac)
-	}
+			eURL := URL("https://www.igdb.com/games/mega-man-8")
+			aURL := g[0].URL
+			if aURL != eURL {
+				t.Errorf("Expected URL '%s', got '%s'", eURL, aURL)
+			}
 
-	ed := 762
-	ad := g[1].Developers[0]
-	if ad != ed {
-		t.Errorf("Expected developer ID %d, got %d", ed, ad)
-	}
+			ec := 1352059102919
+			ac := g[0].CreatedAt
+			if ac != ec {
+				t.Errorf("Expected Unix time in milliseconds of %d, got %d", ec, ac)
+			}
 
-	eID := "etjab1sgankzyq6p6qgf"
-	aID := g[1].Covers.ID
-	if aID != eID {
-		t.Errorf("Expected cloudinary ID '%s', got '%s'", eID, aID)
+			ed := 762
+			ad := g[1].Developers[0]
+			if ad != ed {
+				t.Errorf("Expected developer ID %d, got %d", ed, ad)
+			}
+
+			eID := "etjab1sgankzyq6p6qgf"
+			aID := g[1].Covers.ID
+			if aID != eID {
+				t.Errorf("Expected cloudinary ID '%s', got '%s'", eID, aID)
+			}
+		})
 	}
 }
 
 func TestSearchGames(t *testing.T) {
-	ts, c, err := testServerFile(http.StatusOK, "test_data/search_games.txt")
-	if err != nil {
-		t.Fatal(err)
+	var gameTests = []struct {
+		Name   string
+		Resp   string
+		Qry    string
+		Opts   []OptionFunc
+		ExpErr string
+	}{
+		{"Happy path", "test_data/search_games.txt", "mario", []OptionFunc{OptLimit(50)}, ""},
+		{"Empty query", "test_data/search_games.txt", "", []OptionFunc{OptLimit(50)}, ""},
+		{"Empty response", "test_data/empty.txt", "mario", nil, errEndOfJSON.Error()},
+		{"Invalid option", "test_data/empty.txt", "mario", []OptionFunc{OptOffset(9999)}, ErrOutOfRange.Error()},
 	}
-	defer ts.Close()
+	for _, tt := range gameTests {
+		t.Run(tt.Name, func(t *testing.T) {
+			ts, c, err := testServerFile(http.StatusOK, tt.Resp)
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer ts.Close()
 
-	g, err := c.SearchGames("mario")
-	if err != nil {
-		t.Error(err)
-	}
+			g, err := c.SearchGames(tt.Qry, tt.Opts...)
+			assertError(t, err, tt.ExpErr)
 
-	el := 3
-	al := len(g)
-	if al != el {
-		t.Errorf("Expected length of %d, got %d", el, al)
-	}
+			if tt.ExpErr != "" {
+				return
+			}
 
-	en := "Super Mario 64"
-	an := g[0].Name
-	if an != en {
-		t.Errorf("Expected name '%s', got '%s'", en, an)
-	}
+			el := 3
+			al := len(g)
+			if al != el {
+				t.Errorf("Expected length of %d, got %d", el, al)
+			}
 
-	es := "super-mario-64"
-	as := g[0].Slug
-	if as != es {
-		t.Errorf("Expected slug '%s', got '%s'", es, as)
-	}
+			en := "Super Mario 64"
+			an := g[0].Name
+			if an != en {
+				t.Errorf("Expected name '%s', got '%s'", en, an)
+			}
 
-	er := 85.5273965373542
-	ar := g[1].Rating
-	if ar != er {
-		t.Errorf("Expected rating of %f, got %f", er, ar)
-	}
+			es := "super-mario-64"
+			as := g[0].Slug
+			if as != es {
+				t.Errorf("Expected slug '%s', got '%s'", es, as)
+			}
 
-	ep := 3.666666666666667
-	ap := g[1].Popularity
-	if ap != ep {
-		t.Errorf("Expected popularity of %f, got %f", ep, ap)
-	}
+			er := 85.5273965373542
+			ar := g[1].Rating
+			if ar != er {
+				t.Errorf("Expected rating of %f, got %f", er, ar)
+			}
 
-	ed := 864
-	ad := g[2].Developers[0]
-	if ad != ed {
-		t.Errorf("Expected developer ID %d, got %d", ed, ad)
-	}
+			ep := 3.666666666666667
+			ap := g[1].Popularity
+			if ap != ep {
+				t.Errorf("Expected popularity of %f, got %f", ep, ap)
+			}
 
-	eURL := URL("//images.igdb.com/igdb/image/upload/t_thumb/clmh270eov5rimiggwrk.jpg")
-	aURL := g[2].Covers.URL
-	if aURL != eURL {
-		t.Errorf("Expected URL '%s', got '%s'", eURL, aURL)
+			ed := 864
+			ad := g[2].Developers[0]
+			if ad != ed {
+				t.Errorf("Expected developer ID %d, got %d", ed, ad)
+			}
+
+			eURL := URL("//images.igdb.com/igdb/image/upload/t_thumb/clmh270eov5rimiggwrk.jpg")
+			aURL := g[2].Covers.URL
+			if aURL != eURL {
+				t.Errorf("Expected URL '%s', got '%s'", eURL, aURL)
+			}
+		})
 	}
 }
