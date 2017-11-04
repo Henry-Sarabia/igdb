68943bf708107a55b205ae7adcf2f908da4433bf
Henry-Sarabia <henrysarabia@mail.fresnostate.edu>
2017-11-03 21:37:12 -0700

    Add additional Feed tests

--- feed_test.go
+++ feed_test.go
@@ -23,100 +23,135 @@ func TestFeedTypeIntegrity(t *testing.T) {
 }
 
 func TestGetFeed(t *testing.T) {
-	ts, c, err := testServerFile(http.StatusOK, "test_data/get_feed.txt")
-	if err != nil {
-		t.Fatal(err)
+	var feedTests = []struct {
+		Name   string
+		Resp   string
+		ID     int
+		ExpErr string
+	}{
+		{"Happy path", "test_data/get_feed.txt", 128033, ""},
+		{"Invalid ID", "test_data/empty.txt", -123, ErrNegativeID.Error()},
+		{"Empty Response", "test_data/empty.txt", 128033128033, errEndOfJSON.Error()},
 	}
-	defer ts.Close()
+	for _, tt := range feedTests {
+		t.Run(tt.Name, func(t *testing.T) {
+			ts, c, err := testServerFile(http.StatusOK, tt.Resp)
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer ts.Close()
 
-	f, err := c.GetFeed(132482)
-	if err != nil {
-		t.Error(err)
-	}
+			f, err := c.GetFeed(tt.ID)
+			assertError(t, err, tt.ExpErr)
 
-	eID := 128033
-	aID := f.ID
-	if aID != eID {
-		t.Errorf("Expected ID %d, got %d", eID, aID)
-	}
+			if tt.ExpErr != "" {
+				return
+			}
 
-	eu := 1500917216678
-	au := f.UpdatedAt
-	if au != eu {
-		t.Errorf("Expected Unix time in milliseconds of %d, got %d", eu, au)
-	}
+			eID := 128033
+			aID := f.ID
+			if aID != eID {
+				t.Errorf("Expected ID %d, got %d", eID, aID)
+			}
 
-	eURL := URL("https://www.igdb.com/feed/2qsh")
-	aURL := f.URL
-	if aURL != eURL {
-		t.Errorf("Expected URL '%s', got '%s'", eURL, aURL)
-	}
+			eu := 1500917216678
+			au := f.UpdatedAt
+			if au != eu {
+				t.Errorf("Expected Unix time in milliseconds of %d, got %d", eu, au)
+			}
 
-	eCat := FeedCategory(7)
-	aCat := f.Category
-	if aCat != eCat {
-		t.Errorf("Expected category %d, got %d", eCat, aCat)
-	}
+			eURL := URL("https://www.igdb.com/feed/2qsh")
+			aURL := f.URL
+			if aURL != eURL {
+				t.Errorf("Expected URL '%s', got '%s'", eURL, aURL)
+			}
 
-	em := "{\"aggregator\":\"youtube\",\"external_id\":\"sR1mAv7dcsc\"}"
-	am := f.Meta
-	if am != em {
-		t.Errorf("Expected meta '%s', got '%s'", em, am)
+			eCat := FeedCategory(7)
+			aCat := f.Category
+			if aCat != eCat {
+				t.Errorf("Expected category %d, got %d", eCat, aCat)
+			}
+
+			em := "{\"aggregator\":\"youtube\",\"external_id\":\"sR1mAv7dcsc\"}"
+			am := f.Meta
+			if am != em {
+				t.Errorf("Expected meta '%s', got '%s'", em, am)
+			}
+		})
 	}
 }
 
 func TestGetFeeds(t *testing.T) {
-	ts, c, err := testServerFile(http.StatusOK, "test_data/get_feeds.txt")
-	if err != nil {
-		t.Fatal(err)
+	var feedTests = []struct {
+		Name   string
+		Resp   string
+		IDs    []int
+		Opts   []OptionFunc
+		ExpErr string
+	}{
+		{"Happy path", "test_data/get_feeds.txt", []int{62732, 132484, 143318}, []OptionFunc{OptLimit(5)}, ""},
+		{"Invalid ID", "test_data/empty.txt", []int{-123}, nil, ErrNegativeID.Error()},
+		{"Zero IDs", "test_data/empty.txt", nil, nil, ErrEmptyIDs.Error()},
+		{"Empty Response", "test_data/empty.txt", []int{62732, 132484, 143318}, nil, errEndOfJSON.Error()},
+		{"Invalid option", "test_data/empty.txt", []int{62732, 132484, 143318}, []OptionFunc{OptOffset(9999)}, ErrOutOfRange.Error()},
 	}
-	defer ts.Close()
+	for _, tt := range feedTests {
+		t.Run(tt.Name, func(t *testing.T) {
+			ts, c, err := testServerFile(http.StatusOK, tt.Resp)
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer ts.Close()
 
-	ids := []int{62732, 132484, 143318}
-	f, err := c.GetFeeds(ids)
-	if err != nil {
-		t.Error(err)
+			f, err := c.GetFeeds(tt.IDs, tt.Opts...)
+			assertError(t, err, tt.ExpErr)
+
+			if tt.ExpErr != "" {
+				return
+			}
+
+			el := 3
+			al := len(f)
+			if al != el {
+				t.Errorf("Expected lfth of %d, got %d", el, al)
+			}
+
+			eID := 62732
+			aID := f[0].ID
+			if aID != eID {
+				t.Errorf("Expected ID %d, got %d", eID, aID)
+			}
+
+			em := "eSports Ready - Rainbow Six Siege http://www.youtube.com/watch?v=jRYVzfQz9nU"
+			am := f[0].Content
+			if am != em {
+				t.Errorf("Expected content '%s', got '%s'", em, am)
+			}
+
+			eu := 1501156914070
+			au := f[1].UpdatedAt
+			if au != eu {
+				t.Errorf("Expected Unix time in milliseconds of %d, got %d", eu, au)
+			}
+
+			eURL := URL("https://www.igdb.com/feed/2u84")
+			aURL := f[1].URL
+			if aURL != eURL {
+				t.Errorf("Expected URL '%s', got '%s'", eURL, aURL)
+			}
+
+			eCat := FeedCategory(1)
+			aCat := f[2].Category
+			if aCat != eCat {
+				t.Errorf("Expected category %d, got %d", eCat, aCat)
+			}
+
+			ep := 261098
+			ap := f[2].Pulse
+			if ap != ep {
+				t.Errorf("Expected Pulse ID %d, got %d", ep, ap)
+			}
+		})
 	}
 
-	el := 3
-	al := len(f)
-	if al != el {
-		t.Errorf("Expected lfth of %d, got %d", el, al)
-	}
-
-	eID := 62732
-	aID := f[0].ID
-	if aID != eID {
-		t.Errorf("Expected ID %d, got %d", eID, aID)
-	}
-
-	em := "eSports Ready - Rainbow Six Siege http://www.youtube.com/watch?v=jRYVzfQz9nU"
-	am := f[0].Content
-	if am != em {
-		t.Errorf("Expected content '%s', got '%s'", em, am)
-	}
-
-	eu := 1501156914070
-	au := f[1].UpdatedAt
-	if au != eu {
-		t.Errorf("Expected Unix time in milliseconds of %d, got %d", eu, au)
-	}
-
-	eURL := URL("https://www.igdb.com/feed/2u84")
-	aURL := f[1].URL
-	if aURL != eURL {
-		t.Errorf("Expected URL '%s', got '%s'", eURL, aURL)
-	}
-
-	eCat := FeedCategory(1)
-	aCat := f[2].Category
-	if aCat != eCat {
-		t.Errorf("Expected category %d, got %d", eCat, aCat)
-	}
-
-	ep := 261098
-	ap := f[2].Pulse
-	if ap != ep {
-		t.Errorf("Expected Pulse ID %d, got %d", ep, ap)
-	}
 }
