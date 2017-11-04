50f1524e90ab074c9344645309ba956b52c301bb
Henry-Sarabia <henrysarabia@mail.fresnostate.edu>
2017-11-03 23:53:30 -0700

    Add additional Page tests

--- page_test.go
+++ page_test.go
@@ -23,137 +23,189 @@ func TestPageTypeIntegrity(t *testing.T) {
 }
 
 func TestGetPage(t *testing.T) {
-	ts, c, err := testServerFile(http.StatusOK, "test_data/get_page.txt")
-	if err != nil {
-		t.Fatal(err)
+	var pageTests = []struct {
+		Name   string
+		Resp   string
+		ID     int
+		ExpErr string
+	}{
+		{"Happy path", "test_data/get_page.txt", 8, ""},
+		{"Invalid ID", "test_data/empty.txt", -10, ErrNegativeID.Error()},
+		{"Empty Response", "test_data/empty.txt", 8, errEndOfJSON.Error()},
 	}
-	defer ts.Close()
+	for _, tt := range pageTests {
+		t.Run(tt.Name, func(t *testing.T) {
+			ts, c, err := testServerFile(http.StatusOK, tt.Resp)
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer ts.Close()
 
-	pg, err := c.GetPage(8)
-	if err != nil {
-		t.Error(err)
-	}
+			pg, err := c.GetPage(tt.ID)
+			assertError(t, err, tt.ExpErr)
 
-	en := "IGN"
-	an := pg.Name
-	if an != en {
-		t.Errorf("Expected name '%s', got '%s'", en, an)
-	}
+			if tt.ExpErr != "" {
+				return
+			}
 
-	ep := 2
-	ap := pg.PageFollowCount
-	if ap != ep {
-		t.Errorf("Expected ID %d, got %d", ep, ap)
-	}
+			en := "IGN"
+			an := pg.Name
+			if an != en {
+				t.Errorf("Expected name '%s', got '%s'", en, an)
+			}
 
-	eyt := "https://www.youtube.com/ign"
-	ayt := pg.Youtube
-	if ayt != eyt {
-		t.Errorf("Expected URL '%s', got '%s'", eyt, ayt)
-	}
+			ep := 2
+			ap := pg.PageFollowCount
+			if ap != ep {
+				t.Errorf("Expected ID %d, got %d", ep, ap)
+			}
 
-	ew := 1920
-	aw := pg.Background.Width
-	if aw != ew {
-		t.Errorf("Expected width of %d, got %d", ew, aw)
+			eyt := "https://www.youtube.com/ign"
+			ayt := pg.Youtube
+			if ayt != eyt {
+				t.Errorf("Expected URL '%s', got '%s'", eyt, ayt)
+			}
+
+			ew := 1920
+			aw := pg.Background.Width
+			if aw != ew {
+				t.Errorf("Expected width of %d, got %d", ew, aw)
+			}
+		})
 	}
 }
 
 func TestGetPages(t *testing.T) {
-	ts, c, err := testServerFile(http.StatusOK, "test_data/get_pages.txt")
-	if err != nil {
-		t.Fatal(err)
+	var pageTests = []struct {
+		Name   string
+		Resp   string
+		IDs    []int
+		Opts   []OptionFunc
+		ExpErr string
+	}{
+		{"Happy path", "test_data/get_pages.txt", []int{36, 215}, []OptionFunc{OptLimit(5)}, ""},
+		{"Invalid ID", "test_data/empty.txt", []int{-50}, nil, ErrNegativeID.Error()},
+		{"Zero IDs", "test_data/empty.txt", nil, nil, ErrEmptyIDs.Error()},
+		{"Empty Response", "test_data/empty.txt", []int{36, 215}, nil, errEndOfJSON.Error()},
+		{"Invalid option", "test_data/empty.txt", []int{36, 215}, []OptionFunc{OptOffset(9999)}, ErrOutOfRange.Error()},
 	}
-	defer ts.Close()
+	for _, tt := range pageTests {
+		t.Run(tt.Name, func(t *testing.T) {
+			ts, c, err := testServerFile(http.StatusOK, tt.Resp)
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer ts.Close()
 
-	ids := []int{36, 215}
-	pg, err := c.GetPages(ids)
-	if err != nil {
-		t.Error(err)
-	}
+			pg, err := c.GetPages(tt.IDs, tt.Opts...)
+			assertError(t, err, tt.ExpErr)
 
-	el := 2
-	al := len(pg)
-	if al != el {
-		t.Errorf("Expected length of %d, got %d", el, al)
-	}
+			if tt.ExpErr != "" {
+				return
+			}
 
-	en := "TotalBiscuit, The Cynical Brit"
-	an := pg[0].Name
-	if an != en {
-		t.Errorf("Expected name '%s', got '%s'", en, an)
-	}
+			el := 2
+			al := len(pg)
+			if al != el {
+				t.Errorf("Expected length of %d, got %d", el, al)
+			}
 
-	eyt := "https://www.youtube.com/user/TotalHalibut"
-	ayt := pg[0].Youtube
-	if ayt != eyt {
-		t.Errorf("Expected URL '%s', got '%s'", eyt, ayt)
-	}
+			en := "TotalBiscuit, The Cynical Brit"
+			an := pg[0].Name
+			if an != en {
+				t.Errorf("Expected name '%s', got '%s'", en, an)
+			}
 
-	eu := 1488287514804
-	au := pg[1].UpdatedAt
-	if au != eu {
-		t.Errorf("Expected Unix time in milliseconds of %d, got %d", eu, au)
-	}
+			eyt := "https://www.youtube.com/user/TotalHalibut"
+			ayt := pg[0].Youtube
+			if ayt != eyt {
+				t.Errorf("Expected URL '%s', got '%s'", eyt, ayt)
+			}
 
-	eh := 240
-	ah := pg[1].Logo.Height
-	if ah != eh {
-		t.Errorf("Expected height of %d, got %d", eh, ah)
+			eu := 1488287514804
+			au := pg[1].UpdatedAt
+			if au != eu {
+				t.Errorf("Expected Unix time in milliseconds of %d, got %d", eu, au)
+			}
+
+			eh := 240
+			ah := pg[1].Logo.Height
+			if ah != eh {
+				t.Errorf("Expected height of %d, got %d", eh, ah)
+			}
+		})
 	}
 }
 
 func TestSearchPages(t *testing.T) {
-	ts, c, err := testServerFile(http.StatusOK, "test_data/search_pages.txt")
-	if err != nil {
-		t.Fatal(err)
+	var pageTests = []struct {
+		Name   string
+		Resp   string
+		Qry    string
+		Opts   []OptionFunc
+		ExpErr string
+	}{
+		{"Happy path", "test_data/search_pages.txt", "PC", []OptionFunc{OptLimit(50)}, ""},
+		{"Empty query", "test_data/search_pages.txt", "", []OptionFunc{OptLimit(50)}, ""},
+		{"Empty response", "test_data/empty.txt", "PC", nil, errEndOfJSON.Error()},
+		{"Invalid option", "test_data/empty.txt", "PC", []OptionFunc{OptOffset(9999)}, ErrOutOfRange.Error()},
 	}
-	defer ts.Close()
+	for _, tt := range pageTests {
+		t.Run(tt.Name, func(t *testing.T) {
+			ts, c, err := testServerFile(http.StatusOK, tt.Resp)
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer ts.Close()
 
-	pg, err := c.SearchPages("PC")
-	if err != nil {
-		t.Error(err)
-	}
+			pg, err := c.SearchPages(tt.Qry, tt.Opts...)
+			assertError(t, err, tt.ExpErr)
 
-	el := 2
-	al := len(pg)
-	if al != el {
-		t.Errorf("Expected length of %d, got %d", el, al)
-	}
+			if tt.ExpErr != "" {
+				return
+			}
 
-	eID := 133
-	aID := pg[0].ID
-	if aID != eID {
-		t.Errorf("Expected ID %d, got %d", eID, aID)
-	}
+			el := 2
+			al := len(pg)
+			if al != el {
+				t.Errorf("Expected length of %d, got %d", el, al)
+			}
 
-	ec := CountryCode(826)
-	ac := pg[0].Country
-	if ac != ec {
-		t.Errorf("Expected country code %d, got %d", ec, ac)
-	}
+			eID := 133
+			aID := pg[0].ID
+			if aID != eID {
+				t.Errorf("Expected ID %d, got %d", eID, aID)
+			}
 
-	efb := "https://www.facebook.com/Frankieonpcin1080p"
-	afb := pg[0].Facebook
-	if afb != efb {
-		t.Errorf("Expected Facebook info '%s', got '%s'", efb, afb)
-	}
+			ec := CountryCode(826)
+			ac := pg[0].Country
+			if ac != ec {
+				t.Errorf("Expected country code %d, got %d", ec, ac)
+			}
 
-	eCat := 2
-	aCat := pg[1].Category
-	if aCat != eCat {
-		t.Errorf("Expected category %d, got %d", eCat, aCat)
-	}
+			efb := "https://www.facebook.com/Frankieonpcin1080p"
+			afb := pg[0].Facebook
+			if afb != efb {
+				t.Errorf("Expected Facebook info '%s', got '%s'", efb, afb)
+			}
 
-	ed := "The global authority on PC games. "
-	ad := pg[1].Description
-	if ad != ed {
-		t.Errorf("Expected description '%s', got '%s'", ed, ad)
-	}
+			eCat := 2
+			aCat := pg[1].Category
+			if aCat != eCat {
+				t.Errorf("Expected category %d, got %d", eCat, aCat)
+			}
 
-	etw := "https://twitter.com/pcgamer"
-	atw := pg[1].Twitter
-	if atw != etw {
-		t.Errorf("Expected Twitter info '%s', got '%s'", etw, atw)
+			ed := "The global authority on PC games. "
+			ad := pg[1].Description
+			if ad != ed {
+				t.Errorf("Expected description '%s', got '%s'", ed, ad)
+			}
+
+			etw := "https://twitter.com/pcgamer"
+			atw := pg[1].Twitter
+			if atw != etw {
+				t.Errorf("Expected Twitter info '%s', got '%s'", etw, atw)
+			}
+		})
 	}
 }
