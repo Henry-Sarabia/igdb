ca3249abdb0f30a93aa5a9486b9bcfbee3d7a7c8
Henry-Sarabia <henrysarabia@mail.fresnostate.edu>
2017-11-04 19:58:54 -0700

    Add additional Review tests

--- review_test.go
+++ review_test.go
@@ -24,143 +24,195 @@ func TestReviewTypeIntegrity(t *testing.T) {
 }
 
 func TestGetReview(t *testing.T) {
-	ts, c, err := testServerFile(http.StatusOK, "test_data/get_review.txt")
-	if err != nil {
-		t.Fatal(err)
+	var reviewTests = []struct {
+		Name   string
+		Resp   string
+		ID     int
+		ExpErr string
+	}{
+		{"Happy path", "test_data/get_review.txt", 1462, ""},
+		{"Invalid ID", "test_data/empty.txt", -1000, ErrNegativeID.Error()},
+		{"Empty Response", "test_data/empty.txt", 1462, errEndOfJSON.Error()},
 	}
-	defer ts.Close()
+	for _, tt := range reviewTests {
+		t.Run(tt.Name, func(t *testing.T) {
+			ts, c, err := testServerFile(http.StatusOK, tt.Resp)
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer ts.Close()
 
-	r, err := c.GetReview(1462)
-	if err != nil {
-		t.Error(err)
-	}
+			r, err := c.GetReview(tt.ID)
+			assertError(t, err, tt.ExpErr)
 
-	et := "Almost perfect!"
-	at := r.Title
-	if at != et {
-		t.Errorf("Expected title '%s', got '%s'", et, at)
-	}
+			if tt.ExpErr != "" {
+				return
+			}
 
-	eURL := URL("https://www.igdb.com/games/mario-kart-8/reviews/almost-perfect")
-	aURL := r.URL
-	if eURL != aURL {
-		t.Errorf("Expected URL '%s', got '%s'", eURL, aURL)
-	}
+			et := "Almost perfect!"
+			at := r.Title
+			if at != et {
+				t.Errorf("Expected title '%s', got '%s'", et, at)
+			}
 
-	ev := 250
-	av := r.Views
-	if av != ev {
-		t.Errorf("Expected view count %d, got %d", ev, av)
+			eURL := URL("https://www.igdb.com/games/mario-kart-8/reviews/almost-perfect")
+			aURL := r.URL
+			if eURL != aURL {
+				t.Errorf("Expected URL '%s', got '%s'", eURL, aURL)
+			}
+
+			ev := 250
+			av := r.Views
+			if av != ev {
+				t.Errorf("Expected view count %d, got %d", ev, av)
+			}
+		})
 	}
 }
 
 func TestGetReviews(t *testing.T) {
-	ts, c, err := testServerFile(http.StatusOK, "test_data/get_reviews.txt")
-	if err != nil {
-		t.Fatal(err)
+	var reviewTests = []struct {
+		Name   string
+		Resp   string
+		IDs    []int
+		Opts   []OptionFunc
+		ExpErr string
+	}{
+		{"Happy path", "test_data/get_reviews.txt", []int{1571, 65}, []OptionFunc{OptLimit(5)}, ""},
+		{"Invalid ID", "test_data/empty.txt", []int{-1500}, nil, ErrNegativeID.Error()},
+		{"Zero IDs", "test_data/empty.txt", nil, nil, ErrEmptyIDs.Error()},
+		{"Empty Response", "test_data/empty.txt", []int{1571, 65}, nil, errEndOfJSON.Error()},
+		{"Invalid option", "test_data/empty.txt", []int{1571, 65}, []OptionFunc{OptOffset(9999)}, ErrOutOfRange.Error()},
 	}
-	defer ts.Close()
+	for _, tt := range reviewTests {
+		t.Run(tt.Name, func(t *testing.T) {
+			ts, c, err := testServerFile(http.StatusOK, tt.Resp)
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer ts.Close()
 
-	ids := []int{1571, 65}
-	r, err := c.GetReviews(ids)
-	if err != nil {
-		t.Error(err)
-	}
+			r, err := c.GetReviews(tt.IDs, tt.Opts...)
+			assertError(t, err, tt.ExpErr)
 
-	el := 2
-	al := len(r)
-	if al != el {
-		t.Errorf("Expected length of %d, got %d", el, al)
-	}
+			if tt.ExpErr != "" {
+				return
+			}
 
-	ec := 1
-	ac := r[0].Category
-	if ac != ec {
-		t.Errorf("Expected date category %d, got %d", ec, ac)
-	}
+			el := 2
+			al := len(r)
+			if al != el {
+				t.Errorf("Expected length of %d, got %d", el, al)
+			}
 
-	erc := 3
-	arc := r[0].RatingCategory
-	if arc != erc {
-		t.Errorf("Expected rating category %d, got %d", erc, arc)
-	}
+			ec := 1
+			ac := r[0].Category
+			if ac != ec {
+				t.Errorf("Expected date category %d, got %d", ec, ac)
+			}
 
-	ep := 41
-	ap := r[0].Platform
-	if ap != ep {
-		t.Errorf("Expected platform %d, got %d", ep, ap)
-	}
+			erc := 3
+			arc := r[0].RatingCategory
+			if arc != erc {
+				t.Errorf("Expected rating category %d, got %d", erc, arc)
+			}
 
-	eu := "ZUPERFLY"
-	au := r[1].Username
-	if au != eu {
-		t.Errorf("Expected username '%s', got '%s'", eu, au)
-	}
+			ep := 41
+			ap := r[0].Platform
+			if ap != ep {
+				t.Errorf("Expected platform %d, got %d", ep, ap)
+			}
 
-	ePos := "-smash balls\r\n-Subspace Emissary\r\n-gameplay\r\n-custom levels"
-	aPos := r[1].PositivePoints
-	if aPos != ePos {
-		t.Errorf("Expected positive points '%s', got '%s'", ePos, aPos)
-	}
+			eu := "ZUPERFLY"
+			au := r[1].Username
+			if au != eu {
+				t.Errorf("Expected username '%s', got '%s'", eu, au)
+			}
 
-	eNeg := "-timers\r\n-Mario"
-	aNeg := r[1].NegativePoints
-	if aNeg != eNeg {
-		t.Errorf("Expected negative points '%s', got '%s'", eNeg, aNeg)
+			ePos := "-smash balls\r\n-Subspace Emissary\r\n-gameplay\r\n-custom levels"
+			aPos := r[1].PositivePoints
+			if aPos != ePos {
+				t.Errorf("Expected positive points '%s', got '%s'", ePos, aPos)
+			}
+
+			eNeg := "-timers\r\n-Mario"
+			aNeg := r[1].NegativePoints
+			if aNeg != eNeg {
+				t.Errorf("Expected negative points '%s', got '%s'", eNeg, aNeg)
+			}
+		})
 	}
 }
 
 func TestSearchReviews(t *testing.T) {
-	ts, c, err := testServerFile(http.StatusOK, "test_data/search_reviews.txt")
-	if err != nil {
-		t.Fatal(err)
+	var reviewTests = []struct {
+		Name   string
+		Resp   string
+		Qry    string
+		Opts   []OptionFunc
+		ExpErr string
+	}{
+		{"Happy path", "test_data/search_reviews.txt", "zelda", []OptionFunc{OptLimit(50)}, ""},
+		{"Empty query", "test_data/search_reviews.txt", "", []OptionFunc{OptLimit(50)}, ""},
+		{"Empty response", "test_data/empty.txt", "zelda", nil, errEndOfJSON.Error()},
+		{"Invalid option", "test_data/empty.txt", "zelda", []OptionFunc{OptOffset(9999)}, ErrOutOfRange.Error()},
 	}
-	defer ts.Close()
+	for _, tt := range reviewTests {
+		t.Run(tt.Name, func(t *testing.T) {
+			ts, c, err := testServerFile(http.StatusOK, tt.Resp)
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer ts.Close()
 
-	r, err := c.SearchReviews("zelda")
-	if err != nil {
-		t.Error(err)
-	}
+			r, err := c.SearchReviews(tt.Qry, tt.Opts...)
+			assertError(t, err, tt.ExpErr)
 
-	el := 3
-	al := len(r)
-	if al != el {
-		t.Errorf("Expected length of %d, got %d", el, al)
-	}
+			if tt.ExpErr != "" {
+				return
+			}
 
-	eID := 80
-	aID := r[0].ID
-	if aID != eID {
-		t.Errorf("Expected ID %d, got %d", eID, aID)
-	}
+			el := 3
+			al := len(r)
+			if al != el {
+				t.Errorf("Expected length of %d, got %d", el, al)
+			}
 
-	es := "zelda-reviewathon-number-1"
-	as := r[0].Slug
-	if as != es {
-		t.Errorf("Expected slug '%s', got '%s'", es, as)
-	}
+			eID := 80
+			aID := r[0].ID
+			if aID != eID {
+				t.Errorf("Expected ID %d, got %d", eID, aID)
+			}
 
-	eIn := "In the first review I started explaining my 'Nostalgia Vortex' theory."
-	aIn := r[1].Introduction
-	if !strings.Contains(aIn, eIn) {
-		t.Errorf("Expected Introduction to contain '%s', got '%s'", eIn, aIn)
-	}
+			es := "zelda-reviewathon-number-1"
+			as := r[0].Slug
+			if as != es {
+				t.Errorf("Expected slug '%s', got '%s'", es, as)
+			}
 
-	eCont := "I know every last inch of that country and there is no exit."
-	aCont := r[1].Content
-	if !strings.Contains(aCont, eCont) {
-		t.Errorf("Expected Content to contain '%s', got '%s'", eCont, aCont)
-	}
+			eIn := "In the first review I started explaining my 'Nostalgia Vortex' theory."
+			aIn := r[1].Introduction
+			if !strings.Contains(aIn, eIn) {
+				t.Errorf("Expected Introduction to contain '%s', got '%s'", eIn, aIn)
+			}
 
-	eConc := "but it was also an abrupt change of tone that some gamers might be put off by."
-	aConc := r[2].Conclusion
-	if !strings.Contains(aConc, eConc) {
-		t.Errorf("Expected Conclusion to contain '%s', got '%s'", eConc, aConc)
-	}
+			eCont := "I know every last inch of that country and there is no exit."
+			aCont := r[1].Content
+			if !strings.Contains(aCont, eCont) {
+				t.Errorf("Expected Content to contain '%s', got '%s'", eCont, aCont)
+			}
 
-	elc := 2
-	alc := r[2].Likes
-	if alc != elc {
-		t.Errorf("Expected Likes count %d, got %d", elc, alc)
+			eConc := "but it was also an abrupt change of tone that some gamers might be put off by."
+			aConc := r[2].Conclusion
+			if !strings.Contains(aConc, eConc) {
+				t.Errorf("Expected Conclusion to contain '%s', got '%s'", eConc, aConc)
+			}
+
+			elc := 2
+			alc := r[2].Likes
+			if alc != elc {
+				t.Errorf("Expected Likes count %d, got %d", elc, alc)
+			}
+		})
 	}
 }
