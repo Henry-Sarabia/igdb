8f91f6287c6d011846d38ff8108c50d65d6ee8ac
Henry-Sarabia <henrysarabia@mail.fresnostate.edu>
2017-11-04 19:42:19 -0700

    Add additional Platform tests

--- platform_test.go
+++ platform_test.go
@@ -23,160 +23,212 @@ func TestPlatformTypeIntegrity(t *testing.T) {
 }
 
 func TestGetPlatform(t *testing.T) {
-	ts, c, err := testServerFile(http.StatusOK, "test_data/get_platform.txt")
-	if err != nil {
-		t.Fatal(err)
+	var platformTests = []struct {
+		Name   string
+		Resp   string
+		ID     int
+		ExpErr string
+	}{
+		{"Happy path", "test_data/get_platform.txt", 7, ""},
+		{"Invalid ID", "test_data/empty.txt", -10, ErrNegativeID.Error()},
+		{"Empty Response", "test_data/empty.txt", 7, errEndOfJSON.Error()},
 	}
-	defer ts.Close()
+	for _, tt := range platformTests {
+		t.Run(tt.Name, func(t *testing.T) {
+			ts, c, err := testServerFile(http.StatusOK, tt.Resp)
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer ts.Close()
 
-	p, err := c.GetPlatform(7)
-	if err != nil {
-		t.Error(err)
-	}
+			p, err := c.GetPlatform(tt.ID)
+			assertError(t, err, tt.ExpErr)
 
-	en := "PlayStation"
-	an := p.Name
-	if an != en {
-		t.Errorf("Expected name '%s', got '%s'", en, an)
-	}
+			if tt.ExpErr != "" {
+				return
+			}
 
-	egID := []int{1185, 1186, 1187, 1192, 1195, 1201, 425, 675}
-	agID := p.Games
-	for i := range agID {
-		if agID[i] != egID[i] {
-			t.Errorf("Expected Game ID %d, got %d\n", egID[i], agID[i])
-		}
-	}
+			en := "PlayStation"
+			an := p.Name
+			if an != en {
+				t.Errorf("Expected name '%s', got '%s'", en, an)
+			}
 
-	eCPU := "MIPS R3051 @ 33,8688 MHz"
-	aCPU := p.Versions[0].CPU
-	if aCPU != eCPU {
-		t.Errorf("Expected CPU '%s', got '%s'", eCPU, aCPU)
-	}
+			egID := []int{1185, 1186, 1187, 1192, 1195, 1201, 425, 675}
+			agID := p.Games
+			for i := range agID {
+				if agID[i] != egID[i] {
+					t.Errorf("Expected Game ID %d, got %d\n", egID[i], agID[i])
+				}
+			}
 
-	el := 4
-	al := len(p.Versions[0].ReleaseDates)
-	if al != el {
-		t.Errorf("Expected length of %d, got %d", el, al)
-	}
+			eCPU := "MIPS R3051 @ 33,8688 MHz"
+			aCPU := p.Versions[0].CPU
+			if aCPU != eCPU {
+				t.Errorf("Expected CPU '%s', got '%s'", eCPU, aCPU)
+			}
 
-	ed := 810604800000
-	ad := p.Versions[0].ReleaseDates[1].Date
-	if ad != ed {
-		t.Errorf("Expected Unix time in milliseconds of %d, got %d", ed, ad)
+			el := 4
+			al := len(p.Versions[0].ReleaseDates)
+			if al != el {
+				t.Errorf("Expected length of %d, got %d", el, al)
+			}
+
+			ed := 810604800000
+			ad := p.Versions[0].ReleaseDates[1].Date
+			if ad != ed {
+				t.Errorf("Expected Unix time in milliseconds of %d, got %d", ed, ad)
+			}
+		})
 	}
 }
 
 func TestGetPlatforms(t *testing.T) {
-	ts, c, err := testServerFile(http.StatusOK, "test_data/get_platforms.txt")
-	if err != nil {
-		t.Fatal(err)
+	var platformTests = []struct {
+		Name   string
+		Resp   string
+		IDs    []int
+		Opts   []OptionFunc
+		ExpErr string
+	}{
+		{"Happy path", "test_data/get_platforms.txt", []int{23, 130}, []OptionFunc{OptLimit(5)}, ""},
+		{"Invalid ID", "test_data/empty.txt", []int{-123}, nil, ErrNegativeID.Error()},
+		{"Zero IDs", "test_data/empty.txt", nil, nil, ErrEmptyIDs.Error()},
+		{"Empty Response", "test_data/empty.txt", []int{23, 130}, nil, errEndOfJSON.Error()},
+		{"Invalid option", "test_data/empty.txt", []int{23, 130}, []OptionFunc{OptOffset(9999)}, ErrOutOfRange.Error()},
 	}
-	defer ts.Close()
+	for _, tt := range platformTests {
+		t.Run(tt.Name, func(t *testing.T) {
+			ts, c, err := testServerFile(http.StatusOK, tt.Resp)
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer ts.Close()
 
-	ids := []int{23, 130}
-	p, err := c.GetPlatforms(ids)
-	if err != nil {
-		t.Error(err)
-	}
+			p, err := c.GetPlatforms(tt.IDs, tt.Opts...)
+			assertError(t, err, tt.ExpErr)
 
-	el := 2
-	al := len(p)
-	if al != el {
-		t.Errorf("Expected length of %d, got %d", el, al)
-	}
+			if tt.ExpErr != "" {
+				return
+			}
 
-	en := "Dreamcast"
-	an := p[0].Name
-	if an != en {
-		t.Errorf("Expected name '%s', got '%s'", en, an)
-	}
+			el := 2
+			al := len(p)
+			if al != el {
+				t.Errorf("Expected length of %d, got %d", el, al)
+			}
 
-	ew := 1024
-	aw := p[0].Logo.Width
-	if aw != ew {
-		t.Errorf("Expected width of %d, got %d", ew, aw)
-	}
+			en := "Dreamcast"
+			an := p[0].Name
+			if an != en {
+				t.Errorf("Expected name '%s', got '%s'", en, an)
+			}
 
-	evn := "Initial version"
-	avn := p[0].Versions[0].Name
-	if avn != evn {
-		t.Errorf("Expected name '%s', got '%s'", evn, avn)
-	}
+			ew := 1024
+			aw := p[0].Logo.Width
+			if aw != ew {
+				t.Errorf("Expected width of %d, got %d", ew, aw)
+			}
 
-	eWeb := "http://www.nintendo.com/switch"
-	aWeb := p[1].Website
-	if aWeb != eWeb {
-		t.Errorf("Expected website '%s', got '%s'", eWeb, aWeb)
-	}
+			evn := "Initial version"
+			avn := p[0].Versions[0].Name
+			if avn != evn {
+				t.Errorf("Expected name '%s', got '%s'", evn, avn)
+			}
 
-	eh := 413
-	ah := p[1].Versions[0].Logo.Height
-	if ah != eh {
-		t.Errorf("Expected height of %d, got %d", eh, ah)
-	}
+			eWeb := "http://www.nintendo.com/switch"
+			aWeb := p[1].Website
+			if aWeb != eWeb {
+				t.Errorf("Expected website '%s', got '%s'", eWeb, aWeb)
+			}
 
-	egID := []int{38983, 10232, 885, 11529, 19081, 19175, 29525, 36846, 27081, 26165}
-	agID := p[1].Games
-	for i := range agID {
-		if agID[i] != egID[i] {
-			t.Errorf("Expected Game ID %d, got %d\n", egID[i], agID[i])
-		}
+			eh := 413
+			ah := p[1].Versions[0].Logo.Height
+			if ah != eh {
+				t.Errorf("Expected height of %d, got %d", eh, ah)
+			}
+
+			egID := []int{38983, 10232, 885, 11529, 19081, 19175, 29525, 36846, 27081, 26165}
+			agID := p[1].Games
+			for i := range agID {
+				if agID[i] != egID[i] {
+					t.Errorf("Expected Game ID %d, got %d\n", egID[i], agID[i])
+				}
+			}
+		})
 	}
 }
 
 func TestSearchPlatforms(t *testing.T) {
-	ts, c, err := testServerFile(http.StatusOK, "test_data/search_platforms.txt")
-	if err != nil {
-		t.Fatal(err)
+	var platformTests = []struct {
+		Name   string
+		Resp   string
+		Qry    string
+		Opts   []OptionFunc
+		ExpErr string
+	}{
+		{"Happy path", "test_data/search_platforms.txt", "xbox", []OptionFunc{OptLimit(50)}, ""},
+		{"Empty query", "test_data/search_platforms.txt", "", []OptionFunc{OptLimit(50)}, ""},
+		{"Empty response", "test_data/empty.txt", "xbox", nil, errEndOfJSON.Error()},
+		{"Invalid option", "test_data/empty.txt", "xbox", []OptionFunc{OptOffset(9999)}, ErrOutOfRange.Error()},
 	}
-	defer ts.Close()
+	for _, tt := range platformTests {
+		t.Run(tt.Name, func(t *testing.T) {
+			ts, c, err := testServerFile(http.StatusOK, tt.Resp)
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer ts.Close()
 
-	p, err := c.SearchPlatforms("xbox")
-	if err != nil {
-		t.Error(err)
-	}
+			p, err := c.SearchPlatforms(tt.Qry, tt.Opts...)
+			assertError(t, err, tt.ExpErr)
 
-	el := 2
-	al := len(p)
-	if al != el {
-		t.Errorf("Expected length of %d, got %d", el, al)
-	}
+			if tt.ExpErr != "" {
+				return
+			}
 
-	eID := 11
-	aID := p[0].ID
-	if aID != eID {
-		t.Errorf("Expected ID %d, got %d", eID, aID)
-	}
+			el := 2
+			al := len(p)
+			if al != el {
+				t.Errorf("Expected length of %d, got %d", el, al)
+			}
 
-	ec := 1297639288000
-	ac := p[0].CreatedAt
-	if ac != ec {
-		t.Errorf("Expected Unix time in milliseconds of %d, got %d", ec, ac)
-	}
+			eID := 11
+			aID := p[0].ID
+			if aID != eID {
+				t.Errorf("Expected ID %d, got %d", eID, aID)
+			}
 
-	eg := 6
-	ag := p[0].Generation
-	if ag != eg {
-		t.Errorf("Expected generation %d, got %d", eg, ag)
-	}
+			ec := 1297639288000
+			ac := p[0].CreatedAt
+			if ac != ec {
+				t.Errorf("Expected Unix time in milliseconds of %d, got %d", ec, ac)
+			}
 
-	evl := 3
-	avl := len(p[1].Versions)
-	if avl != evl {
-		t.Errorf("Expected length of %d, got %d", evl, avl)
-	}
+			eg := 6
+			ag := p[0].Generation
+			if ag != eg {
+				t.Errorf("Expected generation %d, got %d", eg, ag)
+			}
 
-	er := "480i, 480p, 720p, 1080i, 1080p"
-	ar := p[1].Versions[1].Resolutions
-	if ar != er {
-		t.Errorf("Expected resolutions '%s', got '%s'", er, ar)
+			evl := 3
+			avl := len(p[1].Versions)
+			if avl != evl {
+				t.Errorf("Expected length of %d, got %d", evl, avl)
+			}
 
-	}
+			er := "480i, 480p, 720p, 1080i, 1080p"
+			ar := p[1].Versions[1].Resolutions
+			if ar != er {
+				t.Errorf("Expected resolutions '%s', got '%s'", er, ar)
 
-	es := "xbox-360-elite"
-	as := p[1].Versions[2].Slug
-	if as != es {
-		t.Errorf("Expected slug '%s', got '%s'", es, as)
+			}
+
+			es := "xbox-360-elite"
+			as := p[1].Versions[2].Slug
+			if as != es {
+				t.Errorf("Expected slug '%s', got '%s'", es, as)
+			}
+		})
 	}
 }
