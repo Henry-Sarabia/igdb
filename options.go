90501b5f31e2d6cb51b307e2f757227357b38bdd
Henry-Sarabia <henrysarabia@mail.fresnostate.edu>
2017-11-28 17:31:51 -0800

    Update options documentation

--- options.go
+++ options.go
@@ -18,30 +18,30 @@ var (
 	ErrEmptySlice = errors.New("igdb.OptionFunc: slice empty")
 	// ErrOutOfRange occurs when a provided number value is out of valid range.
 	ErrOutOfRange = errors.New("igdb.OptionFunc: value out of range")
-	// ErrExclusiveOption occurs when mutually exclusive options are used in a single API call.
-	ErrExclusiveOption = errors.New("igdb.OptionFunc: multiple set options are mutually exclusive")
 	// ErrEmptyQuery occurs when an empty string is used as a query value.
 	ErrEmptyQuery = errors.New("igdb.OptionFunc: query value empty")
 )
 
-// options contains a value map to store optional
-// parameters for the various IGDB API calls.
-// The options type is not accessed directly,
-// it is manipulated using the OptionFunc type.
+// options contains a value map that stores the optional parameters for
+// the various IGDB API calls. The options type is not accessed directly,
+// but instead mutated using the functional options that return an OptionFunc.
 type options struct {
 	Values url.Values
 }
 
-// OptionFunc is used to set the options for an
-// API call. OptionFunc is the first-order function
-// returned by the available functional options
-// (e.g. OptLimit or OptFilter).
+// OptionFunc functions are used to set the options for an API call.
+// OptionFunc is the first-order function returned by the available
+// functional options (e.g. OptLimit or OptFilter). This first-order
+// function is then passed into a service's Get, List, Search, or
+// Count function.
+//
+// Only one of each type of OptionFunc can be set per API call.
+// OptFilter is the only exception to this rule.
 type OptionFunc func(*options) error
 
-// newOpt returns a new options object mutated by
-// the provided OptionFunc arguments. Only one of
-// each type of OptionFunc can be set per API call.
-// OptFilter is the only exception to this rule.
+// newOpt returns a new options object mutated by the provided OptionFunc
+// arguments. If no OptionFunc's are provided, an empty options object is
+// returned.
 func newOpt(ofs ...OptionFunc) (*options, error) {
 	opt := &options{Values: url.Values{}}
 
@@ -55,24 +55,25 @@ func newOpt(ofs ...OptionFunc) (*options, error) {
 	return opt, nil
 }
 
-// order specifies the order in which to place
-// the results from an API call. The two
-// available orders are Ascending and Descending.
+// order specifies the order in which to organize the results from an API call.
+// There are three orders in which results are organized: relevance, ascending,
+// and descending. Relevance is only available as a default and cannot be
+// explicitly specified.
 type order string
 
 // The available orders for the functional option OptOrder.
 const (
 	// OrderAscending is used as an argument in the OptOrder functional
-	// option to set the results from an API call in ascending order.
+	// option to organize the results from an API call in ascending order.
 	OrderAscending order = ":asc"
 	// OrderDescending is used as an argument in the OptOrder functional
-	// option to set the results from an API call in descending order.
+	// option to organize the results from an API call in descending order.
 	OrderDescending order = ":desc"
 )
 
-// OptOrder is a functional option used to set the
-// order of the results from an API call. The default
-// ordering is based on relevance.
+// OptOrder is a functional option used to set the order of the results from
+// an API call, either ascending or descending. The default order is based on
+// relevance and cannot be explicitly set.
 func OptOrder(field string, ord order) OptionFunc {
 	return func(o *options) error {
 		if len(field) == 0 {
@@ -86,9 +87,8 @@ func OptOrder(field string, ord order) OptionFunc {
 	}
 }
 
-// OptLimit is a functional option used to limit
-// the number of results from an API call. The
-// default limit is 10. The maximum limit is 50.
+// OptLimit is a functional option used to limit the number of results from
+// an API call. The default limit is 10. The maximum limit is 50.
 func OptLimit(lim int) OptionFunc {
 	return func(o *options) error {
 		if lim <= 0 || lim > 50 {
@@ -102,9 +102,8 @@ func OptLimit(lim int) OptionFunc {
 	}
 }
 
-// OptOffset is a functional option used to offset
-// results from an API call. The default offset is
-// 0. The maximum offset is 10,000.
+// OptOffset is a functional option used to offset the results from an API
+// call. The default offset is 0. The maximum offset is 10,000.
 func OptOffset(off int) OptionFunc {
 	return func(o *options) error {
 		if off < 0 || off > 50 {
@@ -118,12 +117,13 @@ func OptOffset(off int) OptionFunc {
 	}
 }
 
-// OptFields is a functional option used to
-// specify which fields of the requested IGDB
-// object you want the API to provide. Subfields
-// are accessed with a dot operator. To select all
-// available fields at once, use an asterisk (*)
-// character.
+// OptFields is a functional option used to specify which fields of the
+// requested IGDB object you want the API to provide. Subfields are accessed
+// with a dot operator (e.g. cover.url). To select all available fields at
+// once, use an asterisk character (i.e. *).
+//
+// The default for Get and List functions is set to all available fields.
+// The default for Search functions is set to solely the ID field.
 func OptFields(fields ...string) OptionFunc {
 	return func(o *options) error {
 		if len(fields) == 0 {
@@ -143,10 +143,9 @@ func OptFields(fields ...string) OptionFunc {
 	}
 }
 
-// operator is a postfix operator used in
-// the filter option to filter the results
-// based on the provided field values. For
-// the list of postfix operators, visit:
+// operator represents the postfix operation used to filter the results from
+// an API call using the provided field value. For the list of postfix
+// operators, visit:
 // https://igdb.github.io/api/references/filters/#available-postfixes
 type operator string
 
@@ -178,14 +177,17 @@ const (
 	OpAny operator = "any"
 )
 
-// OptFilter is a functional option used to filter the results from
-// an API call. The provided field name specifies which field to
-// operate on with the provided operator. The given string value
-// represents the value of the field being operated on. OptFilter
-// is the only option allowed to be set multiple times in a single
-// API call. Note, the ID field cannot be used in the filter option
-// except when paired with the OpNotIn operator to filter away
-// specific results.
+// OptFilter is a functional option used to filter the results from an API
+// call. Most filtering operations need three different arguments: an operator
+// and 2 operands. The provided field and val strings act as the operands
+// for the provided operator. Optfilter is the only option allowed to be set
+// multiple times in a single API call. By default, results are unfiltered.
+//
+// Note that the ID field cannot be used for filtering except when paired with
+// the OpNotIn operator to filter away specific results. Also note that when
+// filtering a field that consists of an enumerated type (e.g. Gender Code,
+// Feed Category, Game Status, etc.), you must provide the number corresponding
+// to the intended field value.
 func OptFilter(field string, op operator, val string) OptionFunc {
 	return func(o *options) error {
 		if op == OpExists || op == OpNotExists {
@@ -200,8 +202,7 @@ func OptFilter(field string, op operator, val string) OptionFunc {
 	}
 }
 
-// optSearch is an unexported functional
-// option used to search the IGDB using the
+// optSearch is a functional option used to search the IGDB using the
 // provided query.
 func optSearch(qry string) OptionFunc {
 	return func(o *options) error {
