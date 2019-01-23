80fad6d72b8b501edc10022af5ea1dc5043e91a6
Henry-Sarabia <henrysarabia@mail.fresnostate.edu>
2019-01-22 21:25:48 -0800

    Add AchievementIcon type

---
+++ achievementIcon.go
@@ -0,0 +1,14 @@
+package igdb
+
+//go:generate gomodifytags -file $GOFILE -struct AchievementIcon -add-tags json -w
+
+// AchievementIcon is an icon for a specific achievement.
+// For more information please visit: https://api-docs.igdb.com/#achievement-icon
+type AchievementIcon struct {
+	AlphaChannel bool   `json:"alpha_channel"`
+	Animated     bool   `json:"animated"`
+	Height       int    `json:"height"`
+	ImageID      string `json:"image_id"`
+	URL          string `json:"url"`
+	Width        int    `json:"width"`
+}
