92ab00e1aaa944d4bdad9481177f68e217652214
Henry-Sarabia <henrysarabia@mail.fresnostate.edu>
2019-02-14 22:22:22 -0800

    Fix and add Image tests

--- image_test.go
+++ image_test.go
@@ -1,9 +1,7 @@
-// +build ignore
-
 package igdb
 
 import (
-	"reflect"
+	"github.com/pkg/errors"
 	"testing"
 )
 
@@ -15,32 +13,62 @@ const (
 )
 
 func TestSizedImageURL(t *testing.T) {
-	var imageTests = []struct {
-		Name   string
-		ID     string
-		Size   imageSize
-		Ratio  int
-		ExpURL string
-		ExpErr error
+	var tests = []struct {
+		name    string
+		id      string
+		size    imageSize
+		ratio   int
+		wantURL string
+		wantErr error
 	}{
 		{"Non-empty ID and valid single ratio", testImageID, SizeScreenshotMed, 1, testImageURL, nil},
 		{"Non-empty ID and valid double ratio", testImageID, SizeScreenshotMed, 2, testImageURL2x, nil},
-		{"Empty ID and valid ratio", "", Size1080p, 1, "", ErrBlankID},
 		{"Non-empty ID and invalid negative ratio", testImageID, SizeScreenshotHuge, -1, "", ErrPixelRatio},
 		{"Non-empty ID and invalid triple ratio", testImageID, SizeScreenshotBig, 3, "", ErrPixelRatio},
+		{"Empty ID and valid ratio", "", Size1080p, 1, "", ErrBlankID},
 		{"Empty ID and invalid 0 ratio", "", SizeMicro, 0, "", ErrBlankID},
 	}
 
-	for _, it := range imageTests {
-		t.Run(it.Name, func(t *testing.T) {
-			img := Image{ID: it.ID}
-			url, err := img.SizedURL(it.Size, it.Ratio)
-			if !reflect.DeepEqual(err, it.ExpErr) {
-				t.Fatalf("Expected error '%v', got '%v'", it.ExpErr, err)
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			url, err := SizedImageURL(test.id, test.size, test.ratio)
+			if errors.Cause(err) != test.wantErr {
+				t.Errorf("got: <%v>, want: <%v>", errors.Cause(err), test.wantErr)
 			}
 
-			if url != it.ExpURL {
-				t.Fatalf("Expected url '%s', got '%s'", it.ExpURL, url)
+			if url != test.wantURL {
+				t.Errorf("got: <%v>, want: <%v>", url, test.wantURL)
+			}
+		})
+	}
+}
+
+func TestImage_SizedURL(t *testing.T) {
+	var tests = []struct {
+		name    string
+		image   Image
+		size    imageSize
+		ratio   int
+		wantURL string
+		wantErr error
+	}{
+		{"Non-empty ID and valid single ratio", Image{ImageID: testImageID}, SizeScreenshotMed, 1, testImageURL, nil},
+		{"Non-empty ID and valid double ratio", Image{ImageID: testImageID}, SizeScreenshotMed, 2, testImageURL2x, nil},
+		{"Non-empty ID and invalid negative ratio", Image{ImageID: testImageID}, SizeScreenshotHuge, -1, "", ErrPixelRatio},
+		{"Non-empty ID and invalid triple ratio", Image{ImageID: testImageID}, SizeScreenshotBig, 3, "", ErrPixelRatio},
+		{"Empty ID and valid ratio", Image{}, Size1080p, 1, "", ErrBlankID},
+		{"Empty ID and invalid 0 ratio", Image{}, SizeMicro, 0, "", ErrBlankID},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			url, err := test.image.SizedURL(test.size, test.ratio)
+			if errors.Cause(err) != test.wantErr {
+				t.Errorf("got: <%v>, want: <%v>", errors.Cause(err), test.wantErr)
+			}
+
+			if url != test.wantURL {
+				t.Errorf("got: <%v>, want: <%v>", url, test.wantURL)
 			}
 		})
 	}
